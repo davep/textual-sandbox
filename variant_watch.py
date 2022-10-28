80e1633d980c7471391bf9d20931dda59474879f
Dave Pearson <davep@davep.org>
2022-10-28 12:26:49 +0100

    :sparkles: Add a tester for button variant issue
    Tests https://github.com/Textualize/textual/issues/1048

---
+++ variant_watch.py
@@ -0,0 +1,22 @@
+"""https://github.com/Textualize/textual/issues/1048"""
+
+from textual.app import App, ComposeResult
+from textual.widgets import Button, Static
+
+
+class MyApp( App[None] ):
+
+    def compose( self ) -> ComposeResult:
+        yield Button( "Push Me" )
+        yield Static( "Push the above!", id="classes" )
+
+    def on_button_pressed( self, event: Button.Pressed ) -> None:
+        event.button.variant = (
+            "warning" if event.button.variant == "default" else "default"
+        )
+        self.query_one( "#classes", Static ).update(
+            ", ".join( event.button.classes )
+        )
+
+if __name__ == "__main__":
+    MyApp().run()
