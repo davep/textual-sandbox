7e41720aeeb23c2a5f0749467fcd01f049249ac1
Dave Pearson <davep@davep.org>
2022-10-28 09:11:46 +0100

    :sparkles: Add a quick ID uniqueness tester
    https://github.com/Textualize/textual/discussions/1044

---
+++ id_test.py
@@ -0,0 +1,16 @@
+from textual.app import App
+from textual.widgets import Static
+
+class IDTest(App[int]):
+
+    BINDINGS = [("q","return","Return the result")]
+
+    def compose(self):
+        for _ in range(30):
+            yield Static("The only good bug is a dead bug!", id="quote")
+
+    def action_return(self):
+        self.exit(len(self.query("#quote")))
+
+if __name__ == "__main__":
+    print(f"I found this many quotes: {IDTest().run()}")
