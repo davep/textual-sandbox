a740f6faa5c91cde11f6a4d10c06847c2b6356b7
Dave Pearson <davep@davep.org>
2022-10-28 12:39:58 +0100

    :sparkles: Add an illustration of making an app with arguments
    Relates to https://github.com/Textualize/textual/issues/1030

---
+++ app_with_args.py
@@ -0,0 +1,17 @@
+from textual.app import App, ComposeResult, RenderResult
+from textual.widgets import Static
+
+class Greetings(App[None]):
+
+    def __init__(self, greeting: str="Hello", to_greet: str="World") -> None:
+        self.greeting = greeting
+        self.to_greet = to_greet
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        yield Static(f"{self.greeting}, {self.to_greet}")
+
+if __name__=="__main__":
+    Greetings().run()
+    Greetings(to_greet="davep").run()
+    Greetings("Well hello", "there").run()
