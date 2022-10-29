fcf3f574e89a591ac19583b14fba46703b499faf
Dave Pearson <davep@davep.org>
2022-10-29 11:07:21 +0100

    :sparkles: Add the testing code for issue 1002

---
+++ issue_1002.py
@@ -0,0 +1,33 @@
+from textual.app import App, ComposeResult, RenderResult
+from textual.widgets import Static
+from textual.widget import Widget
+from rich.table import Table
+from rich.text import Text
+
+class Info(Widget):
+    def render(self):
+        table = Table(show_header=False, expand=True, box=None, padding=0)
+        table.add_column(justify="left", no_wrap=True)
+        table.add_column(justify="right", no_wrap=True)
+        table.add_row(
+            Text.from_markup("[b]abc[/]"),
+            Text.from_markup("[green]xyz[/]")
+        )
+        return table
+
+class FooApp(App):
+    CSS = """
+    .box {
+        width: 100%;
+        border: white;
+        height: 1;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Static("One [b]two[/] three", classes="box")
+        yield Info(classes="box")
+
+
+foo_app = FooApp()
+foo_app.run()
