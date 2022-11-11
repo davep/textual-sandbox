60d28008b50a6d8d1c6d65993be49a316817b53c
Dave Pearson <davep@davep.org>
2022-11-11 18:22:02 +0000

    :sparkles: Add tester for animating height

---
+++ animate_height.py
@@ -0,0 +1,32 @@
+"""Currently doesn't work!"""
+
+from textual.app import App, ComposeResult
+from textual.containers import Container
+from textual.css.scalar import Scalar, Unit
+from textual.widgets import Header, Footer
+
+class AniApp( App[ None ] ):
+
+    BINDINGS = [
+        ( "1", "height(1)", "1" ),
+        ( "0", "height(100)", "100" )
+    ]
+
+    CSS = """
+    Container {
+        background: red;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        yield Container( id="box" )
+        yield Footer()
+
+    def action_height( self, pcent: float ) -> None:
+        self.query_one( "#box", Container ).styles.animate(
+            "height", value=Scalar( float( pcent ), Unit.HEIGHT, Unit.HEIGHT ), duration=2.0
+        )
+
+if __name__ == "__main__":
+    AniApp().run()
