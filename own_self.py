99ea7046a572116b38afb737eda55501f5a91bab
Dave Pearson <davep@davep.org>
2022-11-11 17:15:23 +0000

    :sparkles: Add a test for owning self

---
+++ own_self.py
@@ -0,0 +1,23 @@
+"""Bad ID tester.
+
+https://github.com/Textualize/textual/issues/659
+"""
+
+from textual.app import App
+from textual.widget import Widget
+
+class SelfOwn( Widget ):
+
+    def __init__( self ) -> None:
+        super().__init__( self )
+
+
+class SelfOwnage( App[ None ] ):
+
+    def compose(self):
+        yield SelfOwn()
+
+
+if __name__ == "__main__":
+    SelfOwnage().run()
+
