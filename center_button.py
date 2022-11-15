f9fe9b726f925773d7e166a372ccaae13306c999
Dave Pearson <davep@davep.org>
2022-11-15 14:58:24 +0000

    Example of how to center a button on the screen
    See https://fosstodon.org/@nobodyinperson/109348272350243686

---
+++ center_button.py
@@ -0,0 +1,17 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Button
+
+class ButtonApp( App[ None ] ):
+
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Button( "PUSH ME!" )
+
+if __name__ == "__main__":
+    ButtonApp().run()
+
