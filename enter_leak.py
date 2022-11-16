2e94c245fbe68de0c4726119fd1598dbeb5624ae
Dave Pearson <davep@davep.org>
2022-11-16 21:59:58 +0000

    :fire: Reduce the size of the enter leak example

--- enter_leak.py
+++ enter_leak.py
@@ -11,13 +11,9 @@ class EnterLeak( App[ None ] ):
         yield Button( "Press me to try and focus the first field" )
 
     def on_input_submitted( self, event: Input.Submitted ) -> None:
-        print( f"on_input_submitted( {event.input!r})" )
-        if event.input.id == "first":
-           self.set_focus( self.query_one( "#second" ), Input )
-           print( "on_input_submitted is on the #first input and is moving to the #second input" )
-        else:
-           self.set_focus( self.query_one( "#first" ), Input )
-           print( "on_input_submitted is on the #second input and is moving to the #first input" )
+        self.set_focus( self.query_one(
+            "#second" if event.input.id == "first" else "#first"
+        ) )
 
     def on_button_pressed( self, event: Button.Pressed ) -> None:
         self.set_focus( self.query_one( "#first", Input ) )
