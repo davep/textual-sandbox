e893b2f3b842869781e35b7d7687de552317c1cc
Dave Pearson <davep@davep.org>
2022-11-20 08:18:53 +0000

    :sparkles: Add an app that should be transparent
    See https://github.com/Textualize/textual/discussions/1234

---
+++ transparent.py
@@ -0,0 +1,20 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Static
+
+class TransparentScreen( App[ None ] ):
+
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+    Static {
+        width: auto;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Static( "Hello, World!" )
+
+if __name__ == "__main__":
+    TransparentScreen().run()
+
