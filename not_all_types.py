ada02d925eaa15ae65b3ffd4b87e09050f05f23a
Dave Pearson <davep@davep.org>
2022-11-23 08:00:20 +0000

    :sparkles: Illustrate error with numbers in type selectors
    See https://github.com/Textualize/textual/issues/1253

---
+++ not_all_types.py
@@ -0,0 +1,20 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Label
+
+class H1( Label ):
+    pass
+
+class NumberError( App[ None ] ):
+
+    CSS = """
+    H1 {
+        text-style: bold;
+        color: yellow;
+        background: red;
+    }
+    """
+    def compose( self ) -> ComposeResult:
+        yield H1( "This is a very important heading" )
+
+if __name__ == "__main__":
+    NumberError().run()
