926c685c6c165feb4f0d51de9b493fcb250d4e1b
Dave Pearson <davep@davep.org>
2022-11-24 09:37:25 +0000

    :sparkles: Add an example of bindings that throw off an app's display
    For https://github.com/Textualize/textual/issues/1267

---
+++ footgun.py
@@ -0,0 +1,35 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Label, Header, Footer
+from textual.containers import Vertical
+from textual.binding import Binding
+
+class BorkFooter( App[ None ] ):
+
+    TITLE = "Footer Borker"
+
+    BINDINGS = [
+        Binding( "a", "a", "The A key" ),
+        Binding( "backspace", "backspace", "Backspace!" ),
+        Binding( "delete", "delete", "Delete!" )
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Vertical()
+        yield Footer()
+
+    def gndn( self ):
+        self.query_one( Vertical ).mount( Label( "You pressed a key" ) )
+
+    def action_a( self ):
+        self.gndn()
+
+    def action_backspace( self ):
+        self.gndn()
+
+    def action_delete( self ):
+        self.gndn()
+
+if __name__ == "__main__":
+    BorkFooter().run()
+
