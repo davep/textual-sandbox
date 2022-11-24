2621b2118293a8eb2f4634eee4bf498b2ab4c9f7
Dave Pearson <davep@davep.org>
2022-11-24 15:37:10 +0000

    :sparkles: Add an example of the layering issue
    See https://github.com/Textualize/textual/issues/1269

---
+++ layering_issue.py
@@ -0,0 +1,68 @@
+from textual.app import App, ComposeResult
+from textual.screen import Screen
+from textual.widgets import Header, Footer, Label
+from textual.containers import Vertical, Container
+
+class Overlay( Container ):
+
+    def compose( self ) -> ComposeResult:
+        yield Label( "This should float over the top" )
+
+class Body( Vertical ):
+
+    def compose(self) -> ComposeResult:
+         yield Label( "My God! It's full of stars! " * 300 )
+
+class Good( Screen ):
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Overlay()
+        yield Body()
+        yield Footer()
+
+class Bad( Screen ):
+
+    def compose(self) -> ComposeResult:
+        yield Overlay()
+        yield Header()
+        yield Body()
+        yield Footer()
+
+class Layers( App[ None ] ):
+
+    CSS = """
+    Screen {
+        layers: base higher;
+    }
+
+    Overlay {
+        layer: higher;
+        dock: top;
+        width: auto;
+        height: auto;
+        padding: 2;
+        border: solid yellow;
+        background: red;
+        color: yellow;
+    }
+    """
+
+    SCREENS = {
+        "good": Good,
+        "bad": Bad
+    }
+
+    BINDINGS = [
+        ( "t", "toggle", "Toggle Screen" )
+    ]
+
+    def on_mount( self ):
+        self.push_screen( "good" )
+
+    def action_toggle( self ):
+        self.switch_screen( "bad" if self.screen.__class__.__name__ == "Good" else "good" )
+
+if __name__ == "__main__":
+    Layers().run()
+
