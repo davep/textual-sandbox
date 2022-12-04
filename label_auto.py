d41a71a45bd4f6f4fe69cbc187902cb265fc8843
Dave Pearson <davep@davep.org>
2022-12-04 15:18:07 +0000

    Testing label sizing and the like
    See https://github.com/Textualize/textual/discussions/1315

---
+++ label_auto.py
@@ -0,0 +1,67 @@
+"""https://github.com/Textualize/textual/discussions/1315"""
+
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal
+from textual.widgets import Label
+
+class LabelAuto( App[ None ] ):
+
+    CSS = """
+    Horizontal {
+        border: solid blue;
+        height: auto;
+    }
+
+    Label {
+        border: solid red;
+    }
+
+    .width {
+        width: auto;
+    }
+
+    .height {
+        height: auto;
+    }
+
+    .one-fr {
+        width: 1fr;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Horizontal(
+            Label( "Raw Label" ),
+            Label( "Raw Label" ),
+            Label( "Raw Label" ),
+            Label( "Raw Label" ),
+            Label( "Raw Label" )
+        )
+        yield Horizontal(
+            Label( "width: auto;", classes="width" ),
+            Label( "height: auto;", classes="height" ),
+            Label( "width: auto; height: auto;", classes="width height" )
+        )
+        yield Horizontal( Label( "width: auto;", classes="width" ) )
+        yield Horizontal( Label( "height: auto;", classes="height" ) )
+        yield Horizontal( Label( "width: auto; height: auto;", classes="width height" ) )
+        yield Horizontal(
+            Label( "width: auto;", classes="width" ),
+            Label( "width: auto;", classes="width" ),
+            Label( "width: auto;", classes="width" )
+        )
+        yield Horizontal(
+            Label( "width: 1fr;", classes="one-fr" ),
+            Label( "width: 1fr;", classes="one-fr" )
+        )
+        yield Horizontal(
+            Label( "width: 1fr;", classes="one-fr" ),
+            Label( "width: 1fr;", classes="one-fr" ),
+            Label( "width: 1fr;", classes="one-fr" ),
+            Label( "width: 1fr;", classes="one-fr" ),
+            Label( "width: 1fr;", classes="one-fr" ),
+            Label( "width: 1fr;", classes="one-fr" )
+        )
+
+if __name__ == "__main__":
+    LabelAuto().run()
