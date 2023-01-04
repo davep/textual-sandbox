3c09c11551e181e59b1b878c175c5dbb941c1217
Dave Pearson <davep@davep.org>
2023-01-04 19:57:27 +0000

    :sparkles: Couple more tests and a relock

---
+++ double_push.py
@@ -0,0 +1,51 @@
+from textual.app        import App, ComposeResult
+from textual.containers import Grid
+from textual.screen     import Screen
+from textual.widgets    import Header, Footer, Button, Label
+
+class Popper( Screen ):
+
+    def compose( self ) -> ComposeResult:
+        """Compose the child widgets."""
+        yield Label( "WAIT FOR IT!" )
+
+    def on_show( self ) -> None:
+        self.set_timer( 5.0, self.app.pop_screen )
+
+
+class Main( Screen ):
+
+    def compose( self ) -> ComposeResult:
+        """Compose the child widgets."""
+        yield Label( ":-/" )
+        yield Button( "Press me!" )
+
+    def on_show( self ) -> None:
+        self.app.push_screen( "popper" )
+
+    def on_button_pressed( self, _: Button.Pressed ):
+        self.query_one( Label ).update( ":-D" )
+
+class DoublePush( App[ None ] ):
+
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+    """
+
+    SCREENS = {
+        "main": Main,
+        "popper": Popper
+    }
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        yield Button( "Make Screen" )
+        yield Footer()
+
+    def on_button_pressed( self, _: Button.Pressed ):
+        self.push_screen( "main" )
+
+if __name__ == "__main__":
+    DoublePush().run()
---
+++ five_and_rest.py
@@ -0,0 +1,29 @@
+from textual.app        import App, ComposeResult
+from textual.containers import Horizontal, Vertical
+from textual.widgets    import Header, Footer
+
+class FiveAndRestApp( App[ None ] ):
+
+    CSS = """
+    Vertical {
+        border: round red;
+    }
+
+    #left {
+        width: 5;
+    }
+
+    #right {
+        width: 1fr;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        yield Horizontal(
+            Vertical( id="left" ), Vertical( id="right" )
+        )
+        yield Footer()
+
+if __name__ == "__main__":
+    FiveAndRestApp().run()
