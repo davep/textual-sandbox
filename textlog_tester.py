bc5d3ddd20cae81cfd58a585cb6820d7286c076f
Dave Pearson <davep@davep.org>
2023-01-09 10:22:27 +0000

    :sparkles: TextLog tester
    I've seen a few reports of issues with the TextLog, so let's spin up a quick
    tester. So far I'm not seeing the issues anyone has mentioned.

---
+++ textlog_tester.py
@@ -0,0 +1,26 @@
+from textual.app        import App, ComposeResult
+from textual.containers import Vertical
+from textual.widgets    import Header, Footer, TextLog
+from textual.binding    import Binding
+
+class TextLogTester( App[ None ] ):
+
+    BINDINGS = [
+        Binding( str( n ), f"emit({n})", str( n ) ) for n in range( 10 )
+    ]
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        yield Vertical( TextLog( highlight=True, wrap=True ) )
+        yield Footer()
+
+    def on_mount( self ) -> None:
+        self.query_one( TextLog ).write( "This was done on mount" )
+
+    def action_emit( self, how_many: int ) -> None:
+        self.query_one( TextLog ).write(
+            "This 'was' something that was added via an action. " * how_many
+        )
+
+if __name__ == "__main__":
+    TextLogTester().run()
