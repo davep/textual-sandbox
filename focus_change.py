41c7ea2b88c7ef626c2d760809ebdec65a051f1f
Dave Pearson <davep@davep.org>
2023-01-11 16:35:23 +0000

    :sparkles: Add an example of tracking focus change at screen level

---
+++ focus_change.py
@@ -0,0 +1,36 @@
+from textual.app        import App, ComposeResult
+from textual.containers import Vertical
+from textual.screen     import Screen
+from textual.widget     import Widget
+from textual.widgets    import Header, Footer, Input, Label
+
+class TestScreen( Screen ):
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        yield Vertical(
+            Label( "I'll show the focused ID here" ),
+            *[ Input( id=f"input-{n}", placeholder=f"Test input {n}" ) for n in range( 10 ) ]
+        )
+        yield Footer()
+
+    def watch_focused( self, blurring: Widget, focusing: Widget ) -> None:
+        self.query_one( Label ).update(
+            f"Focus went from {blurring} to {focusing}"
+        )
+
+class FocusChaner( App[ None ] ):
+
+    CSS = """
+    Label {
+        height: 3;
+        width: 100%;
+        border: round green;
+    }
+    """
+
+    def on_mount( self ) -> None:
+        self.push_screen( TestScreen() )
+
+if __name__ == "__main__":
+    FocusChaner().run()
