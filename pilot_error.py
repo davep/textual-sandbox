c053ab2fc862db87920d6fa04bb88997c12848ca
Dave Pearson <davep@davep.org>
2023-01-19 10:03:23 +0000

    :sparkles: Add a test for the Pilot freezing
    See https://github.com/Textualize/textual/issues/1608

---
+++ pilot_error.py
@@ -0,0 +1,33 @@
+"""https://github.com/Textualize/textual/issues/1608"""
+
+from asyncio import run
+from textual.app import App
+from textual.screen import Screen
+from textual.widget import Widget
+
+
+class MyWidget(Widget):
+    def compose(self):
+        yield Widget()
+
+
+class MyScreen(Screen):
+
+    def compose(self):
+        yield MyWidget()
+
+
+class MyApp(App):
+
+    def on_mount(self):
+        self.install_screen(MyScreen(), 'myscreen')
+        self.push_screen('myscreen')
+
+
+async def test_this():
+    app = MyApp()
+    async with app.run_test():
+        raise Exception('never raised')
+        assert True
+
+run( test_this() )
