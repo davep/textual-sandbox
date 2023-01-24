ebc33fb9c5e0591ed3bae79672c0186ea5e7fc83
Dave Pearson <davep@davep.org>
2023-01-24 21:10:43 +0000

    :sparkles: Example of how to centre align stuff

---
+++ center_align.py
@@ -0,0 +1,38 @@
+from textual.app        import App, ComposeResult
+from textual.containers import Vertical, Horizontal
+from textual.widgets    import Header, Footer, Button, Label, Input
+
+class CenterApp( App[ None ] ):
+
+    CSS = """
+    Vertical {
+        border: round red;
+        align: center middle;
+    }
+
+    Horizontal {
+        width: 100%;
+        height: auto;
+        align: center middle;
+    }
+
+    Label {
+        border: round yellow;
+    }
+
+    Input {
+        width: 50%;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        yield Vertical(
+            Horizontal( Label( "This is a label" ) ),
+            Horizontal( Button( "This is a button, it's longer" ) ),
+            Horizontal( Input( placeholder="This is an input, it's even longer still" ) )
+        )
+        yield Footer()
+
+if __name__ == "__main__":
+    CenterApp().run()
