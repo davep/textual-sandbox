8d32813dc058d3d6944cdb72b017d5f1806ad64e
Dave Pearson <davep@davep.org>
2023-02-06 09:13:36 +0000

    :hammer: Extend to testing all cursor modes

--- dt_row_msg.py
+++ dt_row_msg.py
@@ -1,8 +1,15 @@
 from textual.app        import App, ComposeResult
 from textual.containers import Vertical
 from textual.widgets    import Header, Footer, TextLog, DataTable
+from textual.binding    import Binding
 
 class TestDataTable( App[ None ] ):
+
+    BINDINGS = [
+        Binding( "c", "cursor( 'cell' )", "Cell" ),
+        Binding( "r", "cursor( 'row' )", "Row" ),
+        Binding( "o", "cursor( 'column' )", "Column" ),
+    ]
 
     CSS = """
     DataTable {
@@ -32,13 +39,31 @@ class TestDataTable( App[ None ] ):
 
     def on_mount(self) -> None:
         self.dt.cursor_type = "row"
-        self.dt.add_columns( "Row", *[ f"Column {n}" for n in range( 10 ) ] )
+        self.dt.add_columns( "Row", *[ f"Column {n+1}" for n in range( 10 ) ] )
         for row in range( 50 ):
             self.dt.add_row( *[ str( row ), *[ str( n ) for n in range( 10 ) ] ] )
         self.dt.focus()
 
-    async def on_data_table_row_selected(self, row_selected: DataTable.RowSelected):
-        self.query_one( TextLog ).write( row_selected )
+    def action_cursor( self, cursor: str ) -> None:
+        self.dt.cursor_type = cursor
+
+    def on_data_table_cell_highlighted( self, event: DataTable.CellHighlighted ) -> None:
+        self.query_one( TextLog ).write( event )
+
+    def on_data_table_cell_selected( self, event: DataTable.CellSelected ) -> None:
+        self.query_one( TextLog ).write( event )
+
+    def on_data_table_row_selected( self, event: DataTable.RowSelected ) -> None:
+        self.query_one( TextLog ).write( event )
+
+    def on_data_table_row_highlighted( self, event: DataTable.RowHighlighted ) -> None:
+        self.query_one( TextLog ).write( event )
+
+    def on_data_table_column_selected( self, event: DataTable.ColumnSelected ) -> None:
+        self.query_one( TextLog ).write( event )
+
+    def on_data_table_column_highlighted( self, event: DataTable.ColumnHighlighted ) -> None:
+        self.query_one( TextLog ).write( event )
 
 if __name__ == "__main__":
     TestDataTable().run()
