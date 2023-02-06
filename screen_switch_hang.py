37d91046494d65aad63df230dd1aa12654edaff2
Dave Pearson <davep@davep.org>
2023-02-06 16:42:31 +0000

    :sparkles: Add a tester for the screen switch issue

---
+++ screen_switch_hang.py
@@ -0,0 +1,36 @@
+"""https://github.com/Textualize/textual/issues/1714"""
+
+from textual.app import App, ComposeResult
+from textual.screen import Screen
+from textual.widgets import Static, Header, Footer
+
+class ScreenA(Screen):
+    BINDINGS = [("b", "switch_to_b", "Switch to screen B")]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Static("A")
+        yield Footer()
+
+    def action_switch_to_b(self):
+        self.app.switch_screen(ScreenB())
+
+class ScreenB(Screen):
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Static("B")
+        yield Footer()
+
+class ModalApp(App):
+    BINDINGS = [("a", "push_a", "Push screen A")]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+
+    def action_push_a(self) -> None:
+        self.push_screen(ScreenA())
+
+
+if __name__ == "__main__":
+    ModalApp().run()
