a52bc70f678a65efb4d8570b6daf33a36977de06
Dave Pearson <davep@davep.org>
2023-02-21 11:07:35 +0000

    Update the toggle tester

--- toggle_boxen.py
+++ toggle_boxen.py
@@ -27,28 +27,35 @@ class ToggleTesterApp( App[ None ] ):
         layout: grid;
         grid-size: 5 10;
     }
+
+    #buttons {
+        height: 3;
+        align: center middle;
+    }
+
+    #buttons > Button {
+        margin-left: 1;
+        margin-right: 1;
+    }
     """
 
     def compose( self ) -> ComposeResult:
         yield Header()
-        yield Horizontal(
-            Vertical(
-                Checkbox( "Checkbox 1" ),
-                Checkbox( "Checkbox 2", button_first=False ),
-                Checkbox( "Checkbox 3" ),
-                Checkbox( "Checkbox 4", id="changer" ),
-            ),
-            RadioSet(
-                RadioButton("Radio Button 1", id="btn1"),
-                RadioButton("Radio Button 2", id="btn2"),
-                RadioButton("Radio Button 3", id="btn3"),
-                RadioButton("Radio Button 4", id="btn4"),
-            ),
-            RadioSet(
-                *[ str( n ) for n in range( 50 ) ], classes="grid"
-            ),
-        )
-        yield Button( "Change" )
+        with Horizontal():
+            with Vertical():
+                yield Checkbox( "Checkbox 1" )
+                yield Checkbox( "Checkbox 2", button_first=False )
+                yield Checkbox( "Checkbox 3" )
+                yield Checkbox( "Checkbox 4", id="changer" )
+            with RadioSet():
+                yield RadioButton("Radio Button 1", id="btn1")
+                yield RadioButton("Radio Button 2", id="btn2")
+                yield RadioButton("Radio Button 3", id="btn3")
+                yield RadioButton("Radio Button 4", id="btn4")
+            yield RadioSet( *[ str( n ) for n in range( 50 ) ], classes="grid" )
+        with Horizontal( id="buttons" ):
+            yield Button( "Change", id="change" )
+            yield Button( "Toggle Disabled", id="disabled" )
         yield TextLog()
         yield Footer()
 
@@ -63,12 +70,19 @@ class ToggleTesterApp( App[ None ] ):
         except NoMatches:
             pass
 
-    def on_button_pressed( self, _: Button.Pressed ) -> None:
-        cb = self.query_one( "#changer", Checkbox )
-        try:
-            cb.label = str( int( str( cb.label ) ) + 1 )
-        except ValueError:
-            cb.label = "0"
+    def on_button_pressed( self, event: Button.Pressed ) -> None:
+        if event.button.id == "change":
+            cb = self.query_one( "#changer", Checkbox )
+            try:
+                cb.label = str( int( str( cb.label ) ) + 1 )
+            except ValueError:
+                cb.label = "0"
+        elif event.button.id == "disabled":
+            self.query_one( ".grid", RadioSet ).disabled = not self.query_one( ".grid", RadioSet ).disabled
+
+    def on_radio_set_changed( self, event: RadioSet.Changed ):
+        self.query_one( TextLog ).write( f"> {event!r}" )
+        self.query_one( TextLog ).write( f">> {event.input.pressed_index} {event.input.pressed_button}" )
 
 if __name__ == "__main__":
     ToggleTesterApp().run()
