f9f4e8c8bc516a74be004ad95427266a2c40dfd7
Dave Pearson <davep@davep.org>
2023-03-02 11:28:43 +0000

    :sparkles: Add a tester for borders
    Gonna start working on adding colour percentage to the CSS border rule and
    need something to play with.

---
+++ percent_border.py
@@ -0,0 +1,34 @@
+from textual.app        import App, ComposeResult
+from textual.containers import Horizontal, Container
+from textual.widgets    import Header, Footer, Label
+
+class BorderPCentApp( App[ None ] ):
+
+    CSS = """
+    Container {
+        height: 1fr;
+        width: 1fr;
+        background: $panel;
+    }
+
+    Container.old-border-type {
+        border: outer red;
+    }
+
+    Container.new-border-type {
+        /* TODO: Add support for a %age after the colour. */
+        /* border: outer red 50%; */
+        border: outer red;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        yield Horizontal(
+            Container( Label( "No border percentage" ), classes="old-border-type" ),
+            Container( Label( "With border percentage" ), classes="new-border-type" ),
+        )
+        yield Footer()
+
+if __name__ == "__main__":
+    BorderPCentApp().run()
