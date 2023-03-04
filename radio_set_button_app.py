d8fb3f6e406d44da830824610f55dfa34625e076
Dave Pearson <davep@davep.org>
2023-03-04 11:08:02 +0000

    :sparkles: Add a couple of RadioSet examples from a discussion

---
+++ radio_set_button_app.py
@@ -0,0 +1,33 @@
+from textual.app     import App, ComposeResult
+from textual.widgets import Header, Footer, RadioSet, RadioButton, Label
+
+class RadioSetButtonApp( App[ None ] ):
+
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+
+    RadioSet {
+        width: 50%;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        with RadioSet():
+            yield RadioButton( "One", name="One" )
+            yield RadioButton( "Two", name="Two" )
+            yield RadioButton( "Three", name="Three" )
+            yield RadioButton( "Four", name="Four" )
+            yield RadioButton( "Five", name="Five" )
+        yield Label( "Press a radio button" )
+        yield Footer()
+
+    def on_radio_set_changed( self, event: RadioSet.Changed ):
+        self.query_one( Label ).update(
+            f"Pressed: {event.pressed.label} named {event.pressed.name}"
+        )
+
+if __name__ == "__main__":
+    RadioSetButtonApp().run()
---
+++ radio_sets.py
@@ -0,0 +1,25 @@
+from textual.app        import App, ComposeResult
+from textual.containers import Vertical
+from textual.widgets    import Header, Footer, RadioSet
+
+class RadioSetsApp( App[ None ] ):
+
+    CSS = """
+    #horizontal {
+        layout: horizontal;
+    }
+    #horizontal RadioButton {
+        margin-right: 1;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        yield Vertical(
+            RadioSet( "One", "Two", "Three", id="default" ),
+            RadioSet( "One", "Two", "Three", id="horizontal" ),
+        )
+        yield Footer()
+
+if __name__ == "__main__":
+    RadioSetsApp().run()
