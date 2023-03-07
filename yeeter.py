0120ac34645ba74b5c0cdf3256abf07d557ae258
Dave Pearson <davep@davep.org>
2023-03-07 20:08:07 +0000

    :zap: Expand the yeeter tester

--- yeeter.py
+++ yeeter.py
@@ -1,6 +1,9 @@
-from textual.app     import App, ComposeResult
-from textual.binding import Binding
-from textual.widgets import Header, Footer, ContentSwitcher, Label
+from __future__ import annotations
+
+from textual.app        import App, ComposeResult
+from textual.containers import Vertical
+from textual.binding    import Binding
+from textual.widgets    import Header, Footer, ContentSwitcher, Label
 
 class YeeterApp( App[ None ] ):
 
@@ -9,27 +12,30 @@ class YeeterApp( App[ None ] ):
         align: center middle;
     }
 
-    Label {
+    ContentSwitcher Label {
         border: solid red;
         background: red;
         color: yellow;
     }
     """
 
-    BINDINGS = [
+    BINDINGS = [ *[
         Binding( f"{n}", f"yeet({n})", f"Child {n}" )
         for n in range( 10 )
-    ]
+    ], Binding( "space", "yeet(None)", "None" )]
 
     def compose( self ) -> ComposeResult:
         yield Header()
-        with ContentSwitcher():
-            for n in range( 10 ):
-                yield Label( f"This is content #{n}", id=f"w{n}" )
+        with Vertical():
+            yield Label("Showing...",id="showing")
+            with ContentSwitcher():
+                for n in range( 10 ):
+                    yield Label( f"This is content #{n}", id=f"w{n}" )
         yield Footer()
 
-    def action_yeet( self, choice ) -> None:
-        self.query_one( ContentSwitcher ).current = f"w{choice}"
+    def action_yeet( self, choice: int | None ) -> None:
+        self.query_one( ContentSwitcher ).current = None if choice is None else f"w{choice}"
+        self.query_one( "#showing", Label ).update( f"{self.query_one( ContentSwitcher ).visible_content!r}")
 
 if __name__ == "__main__":
     YeeterApp().run()
