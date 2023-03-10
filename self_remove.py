725ff830f9d3fad551b04f7978706414aacefd24
Dave Pearson <davep@davep.org>
2023-03-10 12:40:29 +0000

    :sparkles: Test code for https://github.com/Textualize/textual/issues/2016

---
+++ self_remove.py
@@ -0,0 +1,33 @@
+"""https://github.com/Textualize/textual/issues/2016"""
+
+from textual.app        import App, ComposeResult
+from textual.containers import Vertical
+from textual.widgets    import Header, Footer, Button
+
+class SelfRemoveButton( Button ):
+
+    DEFAULT_CSS = """
+    SelfRemoveButton {
+        width: 100%;
+    }
+    """
+
+    def on_button_pressed( self ):
+        self.remove()
+
+class SelfRemoveApp( App[ None ] ):
+
+    BINDINGS = [
+        ( "a", "add", "Add a button" )
+    ]
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        yield Vertical()
+        yield Footer()
+
+    def action_add( self ) -> None:
+        self.query_one( Vertical ).mount( SelfRemoveButton( "Press me to remove me" ) )
+
+if __name__ == "__main__":
+    SelfRemoveApp().run()
