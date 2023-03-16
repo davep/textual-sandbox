685231db91110c4e6df4e3231aa33044ee1a0998
Dave Pearson <davep@davep.org>
2023-03-16 07:40:09 +0000

    :zap: Expand the menu tester a bit more

--- menu_tester.py
+++ menu_tester.py
@@ -1,21 +1,44 @@
-from textual.app     import App, ComposeResult
-from textual.widgets import Header, Footer, Menu
+from rich.panel           import Panel
+from textual.app          import App, ComposeResult
+from textual.widgets      import Header, Footer, Menu, TextLog
+from textual.widgets.menu import MenuOption
+from textual.containers   import Horizontal, Vertical
 
 class MenuTestApp( App[ None ] ):
 
     CSS = """
-    Screen {
-        align: center middle;
+    Horizontal {
+        height: 2fr;
     }
+
+    TextLog {
+        height: 1fr;
+        border: round blue;
+    }
+
     Menu {
-        width: 50%;
-        height: 90%;
+        border: round red;
+        width: 1fr;
+        height: 1fr;
+    }
+
+    *:focus {
+        border: double green;
     }
     """
 
     def compose( self ) -> ComposeResult:
         yield Header()
-        yield Menu[int]()
+        with Vertical():
+            with Horizontal():
+                yield Menu[int](
+                    *[ MenuOption( f"This is option {n}", data=n) for n in range(1_000) ]
+                )
+                yield Menu[None](
+                    *[ Panel( f"This is option {n}") for n in range(1_000) ]
+                )
+                yield Menu[None]()
+            yield TextLog()
         yield Footer()
 
 if __name__ == "__main__":
