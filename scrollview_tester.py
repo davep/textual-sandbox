59e589b5b0d0a9944d637b45ea13c56515e38ce3
Dave Pearson <davep@davep.org>
2023-03-23 16:35:04 +0000

    :sparkles: Add a tester for ScrollView issues
    See https://github.com/Textualize/textual/issues/2126

---
+++ scrollview_tester.py
@@ -0,0 +1,48 @@
+from rich.segment import Segment
+
+from textual.app         import App, ComposeResult
+from textual.widgets     import Header, Footer
+from textual.containers  import Grid
+from textual.scroll_view import ScrollView
+from textual.strip       import Strip
+from textual.geometry    import Size
+
+class TestScrollView( ScrollView, can_focus=True ):
+
+    def __init__( self, height: int, border_title: str ) -> None:
+        super().__init__()
+        self.virtual_size = Size( 0, height )
+        self.border_title = border_title
+
+    def render_line( self, y: int ) -> Strip:
+        return Strip( [
+            Segment( f"Welcome to line {self.scroll_offset.y + y}" ),
+        ] )
+
+class ScrollViewTester( App[ None ] ):
+
+    CSS = """
+    Grid {
+        grid-size: 3;
+    }
+
+    TestScrollView {
+        background: $primary-darken-2;
+        border: round red;
+    }
+
+    TestScrollView:focus {
+        background: $primary;
+        border: double green;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        with Grid():
+            for n in range(3*3):
+                yield TestScrollView(height=1000, border_title=f"{n}")
+        yield Footer()
+
+if __name__ == "__main__":
+    ScrollViewTester().run()
