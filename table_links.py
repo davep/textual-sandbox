6d3a80c9d5ebfe60e02da169c656a9397ccdd3da
Dave Pearson <davep@davep.org>
2023-03-30 09:13:46 +0100

    :sparkles: Code for testing links in tables
    Relevant for when testing the following:
    
      https://github.com/Textualize/textual/issues/2073
      https://github.com/Textualize/textual/issues/2167

---
+++ table_links.py
@@ -0,0 +1,28 @@
+from textual.app        import App, ComposeResult
+from textual.widgets    import Header, Footer, DataTable
+
+class DTLinksApp( App[ None ] ):
+
+    CSS = """
+    DataTable {
+        height: 1fr;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        yield DataTable()
+        yield Footer()
+
+    def on_mount( self ) -> None:
+        dt = self.query_one( DataTable )
+        dt.focus()
+        dt.add_columns( "Title", "URL Markup", "No Markup" )
+        dt.add_rows( [ (
+            f"Relevant XKCD {n}",
+            f"[link]https://xkcd.com/{n}/[/]",
+            f"https://xkcd.com/{n}/"
+        ) for n in range( 500 ) ] )
+
+if __name__ == "__main__":
+    DTLinksApp().run()
