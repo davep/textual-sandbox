041b0b210ea39898cb364aba171a2f22c339245d
Dave Pearson <davep@davep.org>
2023-04-04 08:53:56 +0100

    :sparkles: Example layout for a question on Discord

---
+++ miyu_layout.py
@@ -0,0 +1,41 @@
+from textual.app        import App, ComposeResult
+from textual.containers import Container, Vertical, Horizontal
+from textual.widgets    import Header, Footer
+
+class MiyuLayoutApp( App[ None ] ):
+
+    CSS = """
+    #left {
+        width: 16;
+    }
+
+    #top-box {
+        width: 1fr;
+        height: 8;
+        background: red;
+    }
+
+    #bottom-box {
+        width: 1fr;
+        height: 1fr;
+        background: green;
+    }
+
+    #right {
+        width: 1fr;
+        height: 1fr;
+        background: blue;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        with Horizontal():
+            with Vertical( id="left" ):
+                yield Vertical( id="top-box" )
+                yield Container( id="bottom-box" )
+            yield Container( id="right" )
+        yield Footer()
+
+if __name__ == "__main__":
+    MiyuLayoutApp().run()
