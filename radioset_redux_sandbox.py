b759f001baecc176b9e4f54985a5a435d9c99c15
Dave Pearson <davep@davep.org>
2023-04-25 10:07:29 +0100

    :sparkles: Add a sandbox for RadioSet improvements

---
+++ radioset_redux_sandbox.py
@@ -0,0 +1,29 @@
+from textual.app        import App, ComposeResult
+from textual.containers import Vertical, Horizontal
+from textual.widgets    import Header, Footer, RadioSet, Input
+
+class RadioSetReduxApp( App[ None ] ):
+
+    CSS = """
+    Vertical {
+        border: double green;
+    }
+    RadioSet {
+        width: 1fr;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        with Horizontal():
+            for _ in range( 5 ):
+                with Vertical():
+                    for i in range( 8 ):
+                        if i % 2:
+                            yield RadioSet( *[ f"Choice {n+1}" for n in range( 10 ) ] )
+                        else:
+                            yield Input( placeholder="Some other widget" )
+        yield Footer()
+
+if __name__ == "__main__":
+    RadioSetReduxApp().run()
