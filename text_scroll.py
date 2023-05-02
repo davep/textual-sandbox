baa698ba192c69055af0cecaca48cd6e423752c7
Dave Pearson <davep@davep.org>
2023-05-02 08:50:30 +0100

    :sparkles: Add an example of making scrolling text
    For a question on Discord.

---
+++ text_scroll.py
@@ -0,0 +1,25 @@
+from textual.app        import App, ComposeResult
+from textual.containers import VerticalScroll
+from textual.widgets    import Header, Footer, Label
+
+class ScrollingTextApp( App[ None ] ):
+
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+    #chat {
+        border: solid green;
+        width: 50%;
+        height: 50%;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        yield Header()
+        with VerticalScroll(id="chat"):
+            yield Label( "\n".join( f"This is line number {n}" for n in range( 1_000 ) ) )
+        yield Footer()
+
+if __name__ == "__main__":
+    ScrollingTextApp().run()
