e9a86a6d070d9bc0a4c399d6a8190fcfc9477bb7
Dave Pearson <davep@davep.org>
2023-05-03 15:31:52 +0100

    :hammer: Tweak the tree tester

--- tree_lines_tester.py
+++ tree_lines_tester.py
@@ -9,11 +9,12 @@ class TreeLinesTestApp( App[ None ] ):
 
     CSS = """
     Tree {
-        border: round red;
+        padding: 1 2;
+        border: thick red 20%;
     }
 
     Tree:focus {
-        border: double green;
+        border: thick yellow 50%;
     }
     """
 
@@ -24,15 +25,19 @@ class TreeLinesTestApp( App[ None ] ):
             yield Tree("Root", id="no-root")
         yield Footer()
 
-    def populate( self, node: TreeNode, limit: int=3 ) -> Tree:
+    def populate( self, node: TreeNode, limit: int=4 ) -> Tree:
         for n in range( limit ):
-            self.populate( node.add( str( n ) ), limit-1 )
+            if limit > 1:
+                self.populate( node.add( str( n ) ), limit-1 )
+            else:
+                node.add_leaf( str( n ) )
         return node.tree
 
     def on_mount( self ) -> None:
         self.query_one( "#no-root", Tree ).show_root = False
         self.populate( self.query_one( "#root", Tree ).root ).root.expand_all()
         self.populate( self.query_one( "#no-root", Tree ).root ).root.expand_all()
+        self.query_one( "#root", Tree ).focus()
 
 if __name__ == "__main__":
     TreeLinesTestApp().run()
