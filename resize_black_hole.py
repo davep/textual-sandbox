58dc682930a3eaabac6b4c8f9cd860947f96a4f5
Dave Pearson <davep@davep.org>
2023-05-08 16:21:05 +0100

    :sparkles: Test app for https://github.com/Textualize/textual/issues/2502

---
+++ resize_black_hole.py
@@ -0,0 +1,29 @@
+"""https://github.com/Textualize/textual/issues/2502"""
+
+from textual.app     import App, ComposeResult
+from textual.widgets import Static
+
+class ResizeBlackHoleApp( App[ None ] ):
+
+    CSS = """
+    Screen {
+        layout: horizontal;
+    }
+
+    .fraction {
+        min-width: 3;
+        width: 1fr;
+    }
+
+    .fixed {
+        width: 0;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        for _ in range(100):
+            yield Static(classes="fraction")
+            yield Static(classes="fixed")
+
+if __name__ == "__main__":
+    ResizeBlackHoleApp().run()
