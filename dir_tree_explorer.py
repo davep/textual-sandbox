b35aab117d52a39e6c0e89d307d7678cf9627e63
Dave Pearson <davep@davep.org>
2023-05-11 13:56:11 +0100

    :hammer: Rework the directory tree explorer

--- dir_tree_explorer.py
+++ dir_tree_explorer.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 from textual.app        import App, ComposeResult
 from textual.binding    import Binding
-from textual.containers import Horizontal, Vertical
-from textual.widgets    import Footer, DirectoryTree, Input, Label
+from textual.containers import Grid, Vertical
+from textual.widgets    import Footer, DirectoryTree, Label
 
 class Browser( Vertical ):
 
@@ -25,16 +27,7 @@ class Browser( Vertical ):
     """
 
     def compose( self ) -> ComposeResult:
-        yield Input( placeholder="Enter a new root here" )
-        yield DirectoryTree( "." )
-        yield Label( "Hello!" )
-
-    def on_input_submitted( self, event: Input.Submitted ) -> None:
-        self.query_one( DirectoryTree ).path = event.value
-        self.query_one( DirectoryTree ).focus()
-
-    def on_directory_tree_file_selected( self, event: DirectoryTree.FileSelected ) -> None:
-        self.query_one( Label ).update( f"{event.path!r}" )
+        yield DirectoryTree( Path("~/develop").expanduser() )
 
     def action_reload(self) -> None:
         self.query_one( DirectoryTree ).reload()
@@ -44,15 +37,28 @@ class Browser( Vertical ):
 
 class DirTreeExplorer( App[ None ] ):
 
+    CSS = """
+    Grid {
+        grid-size: 3;
+    }
+    """
+
+    BINDINGS = [
+        Binding( "ctrl+a", "expand_all" ),
+    ]
+
     def compose( self ) -> ComposeResult:
-        with Horizontal():
-            yield Browser()
-            yield Browser()
-            yield Browser()
+        with Grid():
+            for _ in range( 9 ):
+                yield Browser()
         yield Footer()
 
     def on_mount( self ) -> None:
-        self.query( Input ).first().focus()
+        self.query( DirectoryTree ).first().focus()
+
+    def action_expand_all(self):
+        for tree in self.query( DirectoryTree ):
+            tree.root.expand_all()
 
 if __name__ == "__main__":
     DirTreeExplorer().run()
