b4431685dfdda7c9c094ef6c0986df5e78130d12
Dave Pearson <davep@davep.org>
2023-05-22 10:38:37 +0100

    Log messages that I want to see (or not see)

--- selection_list_test.py
+++ selection_list_test.py
@@ -1,6 +1,7 @@
+from textual            import on
 from textual.app        import App, ComposeResult
 from textual.containers import Horizontal
-from textual.widgets    import Header, Footer, SelectionList, TextLog
+from textual.widgets    import Header, Footer, SelectionList, TextLog, OptionList
 
 class SelectionListtestApp( App[ None ] ):
 
@@ -12,6 +13,11 @@ class SelectionListtestApp( App[ None ] ):
             ])
             yield TextLog()
         yield Footer()
+
+    @on( OptionList.OptionHighlighted )
+    @on( OptionList.OptionSelected )
+    def message_reporter( self, event: object ) -> None:
+        self.query_one( TextLog ).write( f"{event!r}" )
 
 if __name__ == "__main__":
     SelectionListtestApp().run()
