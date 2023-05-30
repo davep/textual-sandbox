7fb09c086f77231b8b0b6f14d937e157f21b1e51
Dave Pearson <davep@davep.org>
2023-05-30 08:50:30 +0100

    :sparkles: Testing Screen and on_show
    In response to someone asking questions about it on Discord.

---
+++ screen_and_show.py
@@ -0,0 +1,62 @@
+from textual            import on
+from textual.app        import App, ComposeResult
+from textual.containers import Center, Vertical
+from textual.events     import Show
+from textual.screen     import Screen
+from textual.widgets    import Button, Label, TextLog
+
+class TestScreen( Screen[ None ] ):
+
+    DEFAULT_CSS = """
+    Center {
+        layout: horizontal;
+    }
+
+    Button {
+        margin-left: 1;
+        margin-right: 1;
+    }
+
+    TextLog {
+        border: solid red;
+    }
+
+    TextLog:focus {
+        border: double green;
+    }
+    """
+
+    def __init__( self, depth: int=0 ) -> None:
+        super().__init__()
+        self._depth = depth
+
+    def compose( self ) -> ComposeResult:
+        """Compose the child widgets."""
+        with Vertical():
+            yield Label( f"Depth: {self._depth}")
+            yield TextLog()
+            with Center():
+                yield Button( "Let's go deeper!", id="push" )
+                yield Button( "Let's get out of here!", id="pop" )
+
+    @on( Button.Pressed, "#push")
+    def down( self ) -> None:
+        self.app.push_screen( TestScreen( self._depth + 1 ) )
+
+    @on( Button.Pressed, "#pop")
+    def up( self ) -> None:
+        self.dismiss( None )
+
+    def on_show( self, event: Show ) -> None:
+        self.query_one( TextLog ).write( f"{event!r}" )
+
+class ScreenShowApp( App[ None ] ):
+
+    def finish_up( self, _: None ) -> None:
+        self.exit()
+
+    def on_mount( self ) -> None:
+        self.push_screen( TestScreen(), callback=self.finish_up )
+
+if __name__ == "__main__":
+    ScreenShowApp().run()
