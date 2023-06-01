6e64a560e05eddbe2d9ec45bbdf83ddcff98d7f2
Dave Pearson <davep@davep.org>
2023-06-01 10:17:01 +0100

    :sparkles: Add test code for https://github.com/Textualize/textual/issues/2711

---
+++ windows_timer_bug.py
@@ -0,0 +1,33 @@
+"""https://github.com/Textualize/textual/issues/2711"""
+
+from textual.app        import App, ComposeResult
+from textual.containers import Center
+from textual.reactive   import var
+from textual.widgets    import Label
+
+class WindowsIntervalBugApp( App[ None ] ):
+
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+    """
+
+    counter: var[int] = var(0)
+
+    def compose( self ) -> ComposeResult:
+        yield Center( Label() )
+
+    def watch_counter( self ) -> None:
+        self.query_one( Label ).update( str( self.counter ) )
+
+    def bump_counter( self ) -> None:
+        self.counter += 1
+
+    def on_mount( self ) -> None:
+        self.set_interval( 30, self.bump_counter )
+
+if __name__ == "__main__":
+    WindowsIntervalBugApp().run()
+
+### windows_timer_bug.py ends here
