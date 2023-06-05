2243d05bcf97e1f1b01fa5721c2965feb0776990
Dave Pearson <davep@davep.org>
2023-06-05 08:25:57 +0100

    :zap: Grid/column widget layout question evolved

--- grid_question.py
+++ grid_question.py
@@ -1,20 +1,29 @@
 """https://github.com/Textualize/textual/discussions/2731"""
 
 from textual.app import App, ComposeResult
-from textual.containers import Grid
+from textual.containers import Vertical, Grid
 from textual.widgets import Static
 
 
-class Demo(App):
-    CSS = """Grid {
+class Row(Grid):
+
+    DEFAULT_CSS = """
+    Row {
+        layout: grid;
         grid-size: 2;
     }
     """
+
     def compose(self) -> ComposeResult:
-        with Grid():
+        yield Static("Column 1")
+        yield Static("Column 2")
+
+class Demo(App):
+
+    def compose(self) -> ComposeResult:
+        with Vertical():
             for i in range(9):
-                yield Static("Column 1")
-                yield Static("Column 2")
+                yield Row()
 
 if __name__ == "__main__":
     Demo().run()
