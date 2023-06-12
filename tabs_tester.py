47dd443d3a2a34ee8c34934b21ac0124d0ef89e4
Dave Pearson <davep@davep.org>
2023-06-12 11:09:54 +0100

    :sparkles: Add tests for adding a tab

--- tabs_tester.py
+++ tabs_tester.py
@@ -7,12 +7,13 @@ from textual.widgets    import Tab, Tabs, Label
 class TabsTesterApp( App[ None ] ):
 
     BINDINGS = [
+        Binding( "space", "add" ),
         Binding( "delete", "delete" ),
     ]
 
     def compose( self ) -> ComposeResult:
         yield Tabs(
-            *[ Tab( f"This is tab {n}", id=f"tab-{n}" ) for n in range( 50 ) ]
+            *[ Tab( f"This is tab {n}" ) for n in range( 5 ) ]
         )
         with Center():
             yield Label()
@@ -20,6 +21,9 @@ class TabsTesterApp( App[ None ] ):
     @on( Tabs.TabActivated )
     def show_current_tab( self, event: Tabs.TabActivated ) -> None:
         self.query_one( Label ).update( f"{event.tab!r}" )
+
+    def action_add( self ) -> None:
+        self.query_one( Tabs ).add_tab( "New Tab" )
 
     def action_delete( self ) -> None:
         self.query_one( Tabs ).remove_tab( self.query_one( Tabs ).active_tab )
