e7e1c12f459178a31f22b019eee1b4cea24a7f5d
Dave Pearson <davep@davep.org>
2023-06-12 09:44:18 +0100

    :alembic: Optional CSS path experiment
    Just toying with the idea of loading up a user-supplied stylesheet which can
    be used to "patch" things that might otherwise be hard-coded within the
    source of the application.

---
+++ option_css.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from textual.app     import App, ComposeResult
+from textual.widgets import Label
+
+class OptionalCSSApp( App[ None ] ):
+
+    BINDINGS = [
+        ( "r", "refresh" ),
+    ]
+
+    def compose( self ) -> ComposeResult:
+        yield Label( "This should be optionally changeable." )
+
+    def load_css_from( self, css_file: Path ) -> None:
+        if css_file.exists():
+            new_style_sheet = self.stylesheet.copy()
+            new_style_sheet.read_all( [ css_file ] )
+            new_style_sheet.parse()
+            self.stylesheet = new_style_sheet
+            self.stylesheet.update( self )
+            self.screen.refresh( layout=True )
+
+    def action_refresh( self ) -> None:
+        self.load_css_from( Path( "option_css.css" ) )
+
+if __name__ == "__main__":
+    OptionalCSSApp().run()
