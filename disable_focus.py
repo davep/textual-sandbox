54bea1010d07a4ed3bd652d2ddf1d1f87eae2b3a
Dave Pearson <davep@davep.org>
2023-06-13 14:31:47 +0100

    :sparkles: Example of https://github.com/Textualize/textual/issues/2772

---
+++ disable_focus.py
@@ -0,0 +1,19 @@
+from textual.app        import App, ComposeResult
+from textual.containers import Vertical
+from textual.widgets    import Input
+
+class DisableFocus( App[ None ] ):
+
+    BINDINGS = [
+        ( "ctrl+z", "disable" ),
+    ]
+
+    def compose( self ) -> ComposeResult:
+        with Vertical():
+            yield Input()
+
+    def action_disable( self ) -> None:
+        self.query_one( Vertical ).disabled = not self.query_one( Vertical ).disabled
+
+if __name__ == "__main__":
+    DisableFocus().run()
