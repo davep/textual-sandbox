421bac9e9b720214608e2e2c0903cc19ac41c908
Dave Pearson <davep@davep.org>
2023-06-19 19:52:28 +0100

    :sparkles: Test code for https://github.com/Textualize/textual/issues/2807

---
+++ tabbed_content_remove_bug.py
@@ -0,0 +1,22 @@
+from textual.app import App, ComposeResult
+from textual.widgets import TabbedContent, TabPane, Label, Footer
+
+class TabbedContentRemoveBug(App[None]):
+
+    BINDINGS = [
+        ("delete", "delete"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        with TabbedContent():
+            with TabPane("Keep me", id="keep-me"):
+                yield Label("Keep me")
+            with TabPane("Remove me", id="remove-me"):
+                yield Label("Remove me")
+        yield Footer()
+
+    def action_delete(self) -> None:
+        self.query_one(TabbedContent).remove_pane("remove-me")
+
+if __name__ == "__main__":
+    TabbedContentRemoveBug().run()
