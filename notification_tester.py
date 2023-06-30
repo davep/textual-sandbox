838b60d1873efb741e2c06438d4236c3faa680e8
Dave Pearson <davep@davep.org>
2023-06-30 07:43:31 +0100

    :hammer: Update notification tester for redux work

--- notification_tester.py
+++ notification_tester.py
@@ -1,79 +1,10 @@
 from __future__ import annotations
 
-from subprocess import run
-
-from rich.text import Text
-
 from textual import on
 from textual.app import App, ComposeResult
 from textual.containers import Grid
 from textual.reactive import var
-from textual.screen import Screen
-from textual.widgets import Button, RadioSet, RadioButton
-from textual.widgets.toast import ToastSeverityLevel
-
-class NotificationScreen(Screen):
-
-    notification: var[int] = var(0)
-    via_macos: var[bool] = var(False)
-
-    def compose(self) -> ComposeResult:
-        with Grid():
-            yield RadioSet(
-                RadioButton("Notify Via Textual", value=True),
-                RadioButton("Notify Via macOS")
-            )
-            for _ in range(24):
-                yield Button("Press for a \nnotification")
-
-    def on_mount(self) -> None:
-        for button in self.query(Button):
-            button.tooltip = "Look at this! This is a tooltip for this button!\nCool huh?"
-        self.notify("on_mount")
-
-    @on(RadioSet.Changed)
-    def set_output(self, event: RadioSet.Changed) -> None:
-        self.via_macos = event.index != 0
-
-    def notify(
-        self,
-        message: str,
-        *,
-        title: str | None = None,
-        level: ToastSeverityLevel = "information",
-        timeout: float | None = None,
-    ) -> None:
-        """Show a notification message.
-
-        Args:
-            message: The message to show.
-            title: A title for the notification.
-            level: A severity level for the notification.
-            timeout: A timeout for the notification.
-        """
-        if self.via_macos:
-            title =  f' with title "{title}"' if title else ""
-            run([
-                "osascript",
-                "-e"
-                f'display notification "{Text.from_markup(message)}"{title}'
-            ])
-        else:
-            super().notify(message, title=title, level=level, timeout=timeout)
-
-    @on(Button.Pressed)
-    def show_toast(self) -> None:
-        self.notify(
-            f"This is test notification {self.notification} :smile:\n\n:poop: :poop: :poop: :poop: :poop: :poop: :poop: :poop:",
-            level=["information", "warning", "error"][self.notification % 3],
-            timeout=10,
-            title=[
-                "This was a triumph",
-                "But there's no sense crying over every mistake",
-                "Anyway, this cake is great"
-            ][self.notification % 3],
-        )
-        self.notification += 1
+from textual.widgets import Button
 
 class NotificationTesterApp(App[None]):
 
@@ -92,8 +23,30 @@ class NotificationTesterApp(App[None]):
     }
     """
 
+    notification: var[int] = var(0)
+
+    def compose(self) -> ComposeResult:
+        with Grid():
+            for _ in range(25):
+                yield Button("Press for a \nnotification")
+
     def on_mount(self) -> None:
-        self.push_screen(NotificationScreen())
+        for button in self.query(Button):
+            button.tooltip = "Look at this! This is a tooltip for this button!\nCool huh?"
+        self.app.notify("This was raised in on_mount")
+
+    @on(Button.Pressed)
+    def show_toast(self) -> None:
+        self.app.notify(
+            f"This is test notification {self.notification} :smile:\n\n:poop: :poop: :poop: :poop: :poop: :poop: :poop: :poop:",
+            severity=["information", "warning", "error"][self.notification % 3],
+            title=[
+                "This was a triumph",
+                "But there's no sense crying over every mistake",
+                "Anyway, this cake is great"
+            ][self.notification % 3],
+        )
+        self.notification += 1
 
 if __name__ == "__main__":
     NotificationTesterApp().run()
