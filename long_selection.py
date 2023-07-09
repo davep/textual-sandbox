4d912cfa360669a25357c921f4d2ec59a1307e1d
Dave Pearson <davep@davep.org>
2023-07-09 08:26:40 +0100

    :sparkles: Add code that demonstrates https://github.com/Textualize/textual/issues/2900

---
+++ long_selection.py
@@ -0,0 +1,21 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, SelectionList
+
+
+class TestApp(App):
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+
+        yield SelectionList(
+            ("1 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", 1),
+            ("2 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", 2),
+            ("3 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", 3),
+            ("4 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", 4),
+        )
+
+        yield Footer()
+
+
+if __name__ == "__main__":
+    TestApp().run()
