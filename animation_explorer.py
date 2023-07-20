8d0a50840746d1dc7305e4ce318b0a8ab45a2162
Dave Pearson <davep@davep.org>
2023-07-20 15:27:52 +0100

    :alembic: Experiment with multiple attribute animations

--- animation_explorer.py
+++ animation_explorer.py
@@ -6,32 +6,56 @@ class Counter(Static):
 
     TARGET = 100
 
-    counter: reactive[int] = reactive(0)
+    counter1: reactive[int] = reactive(0)
+    counter2: reactive[int] = reactive(0)
 
     def render(self) -> RenderResult:
-        return f"{self.counter}\n{'=' * int((self.size.width / self.TARGET) * self.counter)}"
+        return (
+            f"{self.counter1}\n"
+            f"{'=' * int((self.size.width / self.TARGET) * self.counter1)}\n\n"
+            f"{self.counter2}\n"
+            f"{'=' * int((self.size.width / self.TARGET) * self.counter2)}"
+        )
 
-    def count(self) -> None:
-        if self.counter in (0, self.TARGET):
+    def count1(self) -> None:
+        if self.counter1 in (0, self.TARGET):
             self.animate(
-                "counter",
-                value = 0 if self.counter else self.TARGET,
+                "counter1",
+                value = 0 if self.counter1 else self.TARGET,
                 duration = 10
             )
         else:
-            self.app.bell()
+            # This is where we should stop the animation.
+            self.counter1 = 0
+            self.count1()
+
+    def count2(self) -> None:
+        if self.counter2 in (0, self.TARGET):
+            self.animate(
+                "counter2",
+                value = 0 if self.counter2 else self.TARGET,
+                duration = 10
+            )
+        else:
+            # This is where we should stop the animation.
+            self.counter2 = 0
+            self.count2()
 
 class AnimationExplorerExample(App[None]):
 
     BINDINGS = [
-        ("space", "count"),
+        ("1", "count1"),
+        ("2", "count2"),
     ]
 
     def compose(self) -> ComposeResult:
         yield Counter()
 
-    def action_count(self) -> None:
-        self.query_one(Counter).count()
+    def action_count1(self) -> None:
+        self.query_one(Counter).count1()
+
+    def action_count2(self) -> None:
+        self.query_one(Counter).count2()
 
 if __name__ == "__main__":
     AnimationExplorerExample().run()
