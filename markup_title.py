28f0697ce18d6b906dc44304442371e6f9620676
Dave Pearson <davep@davep.org>
2023-07-25 09:53:03 +0100

    :sparkles: Testing using markup in an app's title
    It doesn't work, but it can kinda/sorta be made to work.

---
+++ markup_title.py
@@ -0,0 +1,15 @@
+from rich.text import Text
+
+from textual.app import App, ComposeResult
+from textual.widgets import Label, Header
+
+class MarkupTitle(App[None]):
+
+    TITLE = str(Text.from_markup("This is the title :smile:"))
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Label("Hello")
+
+if __name__ == "__main__":
+    MarkupTitle().run()
