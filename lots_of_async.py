62eb4569e3897a375463b92bea5be5bd48872699
Dave Pearson <davep@davep.org>
2023-08-01 11:38:41 +0100

    :hammer: Tidy up the provider/consumer code into a single merge function

--- lots_of_async.py
+++ lots_of_async.py
@@ -1,5 +1,5 @@
 from asyncio import run, sleep, Queue, create_task, wait_for, TimeoutError
-from typing import AsyncIterator, AsyncIterable
+from typing import AsyncIterator, AsyncIterable, Callable
 from random import random
 
 async def provider(prefix: str, count: int) -> AsyncIterator[str]:
@@ -11,20 +11,18 @@ async def consumer(source: AsyncIterable[str], queue: Queue[str]) -> None:
     async for next_item in source:
         await queue.put(next_item)
 
-async def main():
+async def merge_providers(*providers: AsyncIterable[str]) -> AsyncIterator[str]:
     queue = Queue[str]()
-    _ = [
-        create_task(consumer(provider(str(n), 100), queue)) for n in range(10)
-    ]
-    gathered = 0
+    _ = [create_task(consumer(provider, queue)) for provider in providers]
     while True:
         try:
-            print(await wait_for(queue.get(), 0.1))
-            gathered += 1
+            yield await wait_for(queue.get(), 0.1)
         except TimeoutError:
             break
         queue.task_done()
 
-    print(f"Done! Gathered {gathered}")
+async def main() -> None:
+    async for hit in merge_providers(*[provider(str(n), 100) for n in range(10)]):
+        print(hit)
 
 run(main())
