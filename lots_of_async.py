26315d4fd2b4f0bde7e67d23acb3411a0eecebb5
Dave Pearson <davep@davep.org>
2023-08-01 12:54:17 +0100

    :hammer: Check if the tasks are still running before giving up

--- lots_of_async.py
+++ lots_of_async.py
@@ -4,8 +4,8 @@ from random import random
 
 async def provider(prefix: str, count: int) -> AsyncIterator[str]:
     for n in range(count):
+        await sleep(random())
         yield f"{prefix}-{n}"
-        await sleep(random() / 10)
 
 async def consumer(source: AsyncIterable[str], queue: Queue[str]) -> None:
     async for next_item in source:
@@ -13,13 +13,14 @@ async def consumer(source: AsyncIterable[str], queue: Queue[str]) -> None:
 
 async def merge_providers(*providers: AsyncIterable[str]) -> AsyncIterator[str]:
     queue = Queue[str]()
-    _ = [create_task(consumer(provider, queue)) for provider in providers]
-    while True:
+    tasks = [create_task(consumer(provider, queue)) for provider in providers]
+    while any(not task.done() for task in tasks):
         try:
             yield await wait_for(queue.get(), 0.1)
         except TimeoutError:
-            break
-        queue.task_done()
+            pass
+        else:
+            queue.task_done()
 
 async def main() -> None:
     async for hit in merge_providers(*[provider(str(n), 100) for n in range(10)]):
