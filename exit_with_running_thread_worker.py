c94cf734206418fd79a2dd1c8c596d6f3e372215
Dave Pearson <davep@davep.org>
2023-08-02 22:47:05 +0100

    :sparkles: Example of running a tight loop in a threader worker

---
+++ exit_with_running_thread_worker.py
@@ -0,0 +1,27 @@
+from textual import work
+from textual.app import App, ComposeResult
+from textual.widgets import Label, Button
+
+class ExitWithRunningThreadWorkerApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield Label()
+        yield Button("Exit")
+
+    def on_button_pressed(self) -> None:
+        self.exit()
+
+    def on_mount(self) -> None:
+        self.busy_work()
+
+    @work(thread=True)
+    def busy_work(self) -> None:
+        n = 0
+        def _update(n) -> None:
+            self.query_one(Label).update(str(n))
+        while True:
+            self.call_from_thread(_update, n)
+            n += 1
+
+if __name__ == "__main__":
+    ExitWithRunningThreadWorkerApp().run()
