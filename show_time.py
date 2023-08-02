70313fa8f3754331b3bcfe02c5b6ec97028764f9
Dave Pearson <davep@davep.org>
2023-08-02 08:17:47 +0100

    :sparkles: An example of a self-updating widget, for a question in Discord

---
+++ show_time.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from textual.app import App, ComposeResult, RenderableType
+from textual.widgets import Label
+
+class Time(Label):
+
+    def on_mount(self) -> None:
+        self.auto_refresh = 0.1
+
+    def render(self) -> RenderableType:
+        return str(datetime.now())
+
+class TimeApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield Time()
+
+if __name__ == "__main__":
+    TimeApp().run()
