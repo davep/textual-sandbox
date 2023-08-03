4d2cb97f514f3d419ae6c833746c41fe4181ef50
Dave Pearson <davep@davep.org>
2023-08-03 11:08:58 +0100

    :sparkles: Add an example of https://github.com/Textualize/textual/issues/3053

---
+++ focus_in_invisible.py
@@ -0,0 +1,29 @@
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal, Vertical
+from textual.widgets import Button, Label
+
+class InvisibleContainerFocusApp(App[None]):
+
+    CSS = """
+    #invisible {
+        visibility: hidden;
+    }
+
+    #invisible > * {
+        visibility: visible;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            with Vertical():
+                yield Label("The container for this is visible")
+                for _ in range(10):
+                    yield Button()
+            with Vertical(id="invisible"):
+                yield Label("The container for this is invisible")
+                for _ in range(10):
+                    yield Button()
+
+if __name__ == "__main__":
+    InvisibleContainerFocusApp().run()
