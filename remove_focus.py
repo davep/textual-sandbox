36b1511b3b1c31d1709857147188c6420fc18d20
Dave Pearson <davep@davep.org>
2023-08-16 08:38:29 +0100

    :sparkles: Add an example of setting focus to nothing from code
    Rather than using AUTO_FOCUS. Personally I'd use AUTO_FOCUS but the question
    was asked.

---
+++ remove_focus.py
@@ -0,0 +1,15 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Input, Button
+
+class RemoveFocusApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        for _ in range(5):
+            yield Input()
+            yield Button()
+
+    def on_mount(self) -> None:
+        self.call_after_refresh(self.screen.set_focus, None)
+
+if __name__ == "__main__":
+    RemoveFocusApp().run()
