9f18727b030c2d3cefa0cd14e7361660a9304bbb
Dave Pearson <davep@davep.org>
2023-08-24 12:18:20 +0100

    :sparkles: Add an example of updating a label of a Button

---
+++ button_clicker.py
@@ -0,0 +1,33 @@
+from textual import on
+from textual.app import App, ComposeResult
+from textual.widgets import Button
+
+class TheBestGameEver(App[None]):
+
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+
+    Button {
+        width: 50%;
+        height: 50%;
+    }
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.score = 0
+
+    def compose(self) -> ComposeResult:
+        yield Button(str(self.score))
+
+    @on(Button.Pressed)
+    def clicker(self) -> None:
+        self.score += 1
+        self.query_one(Button).label = str(self.score)
+
+if __name__ == "__main__":
+    TheBestGameEver().run()
+
+### button_clicker.py ends here
