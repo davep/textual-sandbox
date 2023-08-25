35c9f1994fd4acdba5dedeff3ea5d22aadf6b66f
Dave Pearson <davep@davep.org>
2023-08-25 07:44:45 +0100

    :sparkles: An example of running something after Textual exits
    For a question on Discord.

---
+++ run_after_exit.py
@@ -0,0 +1,19 @@
+from textual import on
+from textual.app import App, ComposeResult
+from textual.widgets import Button, Label
+
+class RunAfterExitapp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield Label("Press this button to exit")
+        yield Button("Yes, that's right, this button")
+
+    @on(Button.Pressed)
+    def bye(self) -> None:
+        self.exit()
+
+if __name__ == "__main__":
+    RunAfterExitapp().run()
+    print("Hey! Welcome back!")
+
+### run_after_exit.py ends here
