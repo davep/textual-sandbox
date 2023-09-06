a4df46b0f8f809ef5bd0dbad9e96acbc30cbf2e4
Dave Pearson <davep@davep.org>
2023-09-06 14:10:10 +0100

    :sparkles: Couple of examples of stuff from today
    Yes, it's too hot for me to care about writing good commit messages.

---
+++ force_watch.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+from textual import on
+from textual.app import App, ComposeResult
+from textual.reactive import var
+from textual.widgets import Button, RichLog
+
+@dataclass
+class ComplexThing:
+
+    value: int = 0
+
+class ForceWatchApp(App[None]):
+
+    thing: var[ComplexThing] = var(ComplexThing, always_update=True)
+
+    def compose(self) -> ComposeResult:
+        yield Button("Push me to update the property")
+        yield RichLog()
+
+    def watch_thing(self) -> None:
+        self.query_one(RichLog).write(f"Changed {self.thing!r}")
+
+    @on(Button.Pressed)
+    def update_thing(self) -> None:
+        self.thing.value += 1
+        self.thing = self.thing
+
+if __name__ == "__main__":
+    ForceWatchApp().run()
