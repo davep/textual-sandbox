0e94bac5cadee402c0a36212ae8bebef6384b5a2
Dave Pearson <davep@davep.org>
2023-09-10 20:56:09 +0100

    :sparkles: Example of updating a table from a thread worker
    For a question on Discord.

---
+++ worker_messages.py
@@ -0,0 +1,61 @@
+from time import sleep
+from dataclasses import dataclass
+
+from textual import on, work
+from textual.app import App, ComposeResult
+from textual.message import Message
+from textual.widgets import DataTable, Log
+from textual.worker import get_current_worker
+
+class BusyTable(DataTable):
+
+    class Starting(Message):
+        pass
+
+    @dataclass
+    class Updating(Message):
+        on_step: int
+
+    class Finished(Message):
+        pass
+
+    def on_mount(self) -> None:
+        self.add_columns("Col 1", "Col 2")
+        self.load_data()
+
+    @work(thread=True)
+    def load_data(self) -> None:
+        worker = get_current_worker()
+        self.post_message(self.Starting())
+        for n in range(100):
+            if worker.is_cancelled:
+                break
+            else:
+                sleep(3)
+                self.post_message(self.Updating(n))
+        self.post_message(self.Finished())
+
+    @on(Updating)
+    def update_data(self, event: Updating) -> None:
+        self.add_row("New row", str(event.on_step))
+
+class WorkerStateChangeApp(App[None]):
+
+    CSS = """
+    Screen > * {
+        height: 1fr;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield BusyTable()
+        yield Log()
+
+    @on(BusyTable.Starting)
+    @on(BusyTable.Updating)
+    @on(BusyTable.Finished)
+    def log_message(self, event: Message) -> None:
+        self.query_one(Log).write_line(f"{event!r}")
+
+if __name__ == "__main__":
+    WorkerStateChangeApp().run()
