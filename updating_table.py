134ff5c9d36afbdb61e87b87e1497dbd248cf20d
Dave Pearson <davep@davep.org>
2023-09-17 08:06:44 +0100

    :sparkles: Example for https://github.com/Textualize/textual/discussions/3328

---
+++ updating_table.py
@@ -0,0 +1,29 @@
+"""https://github.com/Textualize/textual/discussions/3328"""
+
+from random import randint, random
+
+from textual.app import App, ComposeResult
+from textual.widgets import DataTable
+
+class UpdatingTableExampleApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield DataTable()
+
+    def on_mount(self) -> None:
+
+        table = self.query_one(DataTable)
+        table.add_column("Parameter", key="param")
+        table.add_column("Value", key="value")
+        for n in range(50):
+            table.add_row(f"Paramater {n}", 0, key=f"param{n}")
+
+        self.set_interval(0.1, self.update_table)
+
+    def update_table(self) -> None:
+        table = self.query_one(DataTable)
+        row = randint(0, 49)
+        table.update_cell(f"param{row}", "value", random())
+
+if __name__ == "__main__":
+    UpdatingTableExampleApp().run()
