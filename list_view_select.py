ede2ee4899dd3fc808a80582079404c75ea42059
Dave Pearson <davep@davep.org>
2023-09-24 19:02:29 +0100

    :sparkles: Add a ListView.Selected example for Discord

---
+++ list_view_select.py
@@ -0,0 +1,28 @@
+"""Example of handling ListView.Selected for a question on Discord."""
+
+from textual import on
+from textual.app import App, ComposeResult
+from textual.widgets import ListView, ListItem, Label
+
+class ListViewMessageExampleApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield Label("Chosen item will appear here", id="chosen")
+        yield ListView(
+            ListItem(Label("Mal"), id="mal"),
+            ListItem(Label("Zoe"), id="zoe"),
+            ListItem(Label("Wash"), id="wash"),
+            ListItem(Label("Jayne"), id="jayne"),
+            ListItem(Label("Book"), id="book"),
+            ListItem(Label("Simon"), id="simon"),
+            ListItem(Label("River"), id="river"),
+        )
+
+    @on(ListView.Selected)
+    def show_chosen(self, event: ListView.Selected) -> None:
+        self.query_one("#chosen", Label).update(f"{event.item}")
+
+if __name__ == "__main__":
+    ListViewMessageExampleApp().run()
+
+### list_view_select.py ends here
