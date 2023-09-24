f370d13b7d2e47693c2c34ae94d89d12df30cf93
Dave Pearson <davep@davep.org>
2023-09-24 23:18:12 +0100

    :sparkles: Example for https://github.com/Textualize/textual/discussions/3394

---
+++ show_hide_example.py
@@ -0,0 +1,39 @@
+"""https://github.com/Textualize/textual/discussions/3394"""
+
+from textual import on
+from textual.app import App, ComposeResult
+from textual.containers import Vertical
+from textual.widgets import Select, Static, Input
+
+class ShowHideExampleApp(App[None]):
+
+    CSS = """
+    Vertical > Static {
+        height: 1fr;
+        background: red;
+    }
+
+    .hidden {
+        display: none;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        with Vertical():
+            yield Select(
+                (("Show", "show"), ("Hide", "hide")),
+                value="show", allow_blank=False
+            )
+            yield Static("Show/hide me")
+            yield Input(placeholder="This comes after")
+
+    @on(Select.Changed)
+    def show_hide(self, event: Select.Changed) -> None:
+        self.query_one("Vertical > Static").set_class(
+            event.value == "hide", "hidden"
+        )
+
+if __name__ == "__main__":
+    ShowHideExampleApp().run()
+
+### show_hide_example.py ends here
