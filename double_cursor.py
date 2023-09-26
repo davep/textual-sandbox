c7ea13f1eed4d2277bd4744b2b111a25abd83ec9
Dave Pearson <davep@davep.org>
2023-09-26 14:05:36 +0100

    :sparkles: Example for the double cursor issue
    See https://github.com/Textualize/textual/issues/3405

---
+++ double_cursor.py
@@ -0,0 +1,10 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Input
+
+class DoubleCursorApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield Input()
+
+if __name__ == "__main__":
+    DoubleCursorApp().run()
