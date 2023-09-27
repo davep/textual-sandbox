10b44a87b4fdce2ad0bbb2cf55ae1cfade87f426
Dave Pearson <davep@davep.org>
2023-09-27 09:39:15 +0100

    :sparkles: Example of an animated label offset

---
+++ label_offset.py
@@ -0,0 +1,24 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Label
+
+class LabelOffsetExample(App[None]):
+
+    CSS = """
+    Label {
+        transition: offset 500ms linear;
+    }
+
+    Label.other-spot {
+        offset: 20 20;
+    }
+    """
+
+    BINDINGS = [
+        ("space", "toggle_class('Label', 'other-spot')"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Label("Here is a label!")
+
+if __name__ == "__main__":
+    LabelOffsetExample().run()
