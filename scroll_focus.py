764544274f43b77ba9b529c81eba86e3c798db11
Dave Pearson <davep@davep.org>
2023-09-28 13:57:53 +0100

    :sparkles: Different uses for scrolling
    For a question on GitHub

---
+++ scroll_focus.py
@@ -0,0 +1,20 @@
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal, VerticalScroll
+from textual.widgets import Button, Static
+
+class ScrollAndFocusApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            with VerticalScroll():
+                for n in range(256):
+                    yield Static(f"This is widget {n}")
+            with VerticalScroll():
+                yield Static("\n".join([f"This is line {n}" for n in range(256)]))
+            with VerticalScroll() as buttons:
+                buttons.can_focus = False
+                for n in range(256):
+                    yield Button(f"This is button {n}")
+
+if __name__ == "__main__":
+    ScrollAndFocusApp().run()
