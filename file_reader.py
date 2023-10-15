fe89d5ca505db9d479ad4f9e8af4b1a90e6b6fe6
Dave Pearson <davep@davep.org>
2023-10-15 10:16:36 +0100

    :sparkles: Example code for a question on Discord

---
+++ file_reader.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from textual import work
+from textual.app import App, ComposeResult
+from textual.screen import Screen
+from textual.widgets import Header, Log, Footer
+
+class ShowFile(Screen):
+
+    def __init__(self, path: Path, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.path = path
+
+    def on_mount(self) -> None:
+        self.sub_title = f"Showing: {self.path.name}"
+        self.read_file(self.path)
+
+    def compose(self) -> ComposeResult:
+        yield Header(show_clock=True)
+        yield Log(classes="status-richlog")
+        yield Footer()
+
+    @work(thread=True)
+    def read_file(self, path: Path) -> None:
+        log = self.query_one(Log)
+        with open(path, "r") as f:
+            for line in f:
+                self.app.call_from_thread(log.write, line)
+
+class LoadUpFileApp(App[None]):
+
+    def on_mount(self) -> None:
+        self.push_screen(ShowFile(Path(__file__)))
+
+if __name__ == "__main__":
+    LoadUpFileApp().run()
+
+### file_reader.py ends here
