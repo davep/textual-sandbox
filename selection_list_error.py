b2674700aa9c4d3de88be9a7ec26773b32651b18
Dave Pearson <davep@davep.org>
2023-10-19 20:37:46 +0100

    :sparkles: Exploring https://github.com/Textualize/textual/discussions/3559

---
+++ selection_list_error.py
@@ -0,0 +1,18 @@
+"""https://github.com/Textualize/textual/discussions/3559"""
+
+from textual.app import App, ComposeResult
+from textual.widgets import SelectionList
+from textual.widgets.selection_list import Selection
+
+class SelectionListError(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield SelectionList[str](Selection("Test", "test", id="test"))
+
+    def on_mount(self) -> None:
+        self.query_one(SelectionList).remove_option("test")
+
+if __name__ == "__main__":
+    SelectionListError().run()
+
+### selection_list_error.py ends here
