1dec0da43191a9c0640f2973ed7f90c8ab9aab58
Dave Pearson <davep@davep.org>
2023-10-27 09:00:01 +0100

    :sparkles: Example for a question on Discord

---
+++ label_and_input.py
@@ -0,0 +1,37 @@
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal
+from textual.widgets import Label, Input
+
+class LabelAndInput(Horizontal):
+
+    DEFAULT_CSS = """
+    LabelAndInput {
+        height: auto;
+    }
+
+    LabelAndInput Input {
+        width: 1fr;
+    }
+
+    LabelAndInput Label {
+        height: 3;
+        content-align: right middle;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        """Compose the child widgets."""
+        yield Label("Label")
+        yield Input()
+
+class LabelAndInputWidgetApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield LabelAndInput()
+        yield LabelAndInput()
+        yield LabelAndInput()
+
+if __name__ == "__main__":
+    LabelAndInputWidgetApp().run()
+
+### label_and_input.py ends here
