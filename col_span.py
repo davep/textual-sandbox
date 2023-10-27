3f4d271d030f5ecc5631eba37ca69532ceb04022
Dave Pearson <davep@davep.org>
2023-10-27 09:00:40 +0100

    :sparkles: Testing for a possible bug with grid

---
+++ col_span.py
@@ -0,0 +1,29 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Label
+
+class ColSpanApp(App[None]):
+
+    CSS = """
+    Screen {
+        layout: grid;
+        grid-size: 2;
+    }
+
+    Label {
+        width: 1fr;
+        border: solid green;
+    }
+
+    #label2 {
+        column-span: 2;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Label("Label 1", id="label1")
+        yield Label("Label 2", id="label2")
+
+if __name__ == "__main__":
+    ColSpanApp().run()
+
+### col_span.py ends here
