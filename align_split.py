f8d4d666c1fa47a687361bbedc58d49a1f4dcb11
Dave Pearson <davep@davep.org>
2023-10-29 21:29:53 +0000

    :sparkles: Example of aligning things left/right
    For a question on Discord.

---
+++ align_split.py
@@ -0,0 +1,25 @@
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal
+from textual.widgets import Button, Checkbox
+
+class AlignSplitApp(App[None]):
+
+    CSS = """
+    Horizontal.right {
+        align-horizontal: right;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            with Horizontal(classes="left"):
+                yield Button("Save")
+                yield Button("Load")
+            with Horizontal(classes="right"):
+                yield Button("Start")
+                yield Checkbox("Convert to mp3")
+
+if __name__ == "__main__":
+    AlignSplitApp().run()
+
+### align_split.py ends here
