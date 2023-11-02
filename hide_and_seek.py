f571d3d00d498c7e465df7ebf2f064d194f0969d
Dave Pearson <davep@davep.org>
2023-11-02 15:50:31 +0000

    :sparkles: Example of hiding/showing a widget based on a selection
    For a question on Discord.

---
+++ hide_and_seek.py
@@ -0,0 +1,27 @@
+from textual import on
+from textual.app import App, ComposeResult
+from textual.widgets import Label, Select
+
+class HideAndSeek(App[None]):
+
+    CSS = """
+    .hidden {
+        visibility: hidden;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Label("Here I am!", classes="hidden")
+        yield Select[bool](
+            (("Hidden", False), ("Visible", True)), value=False, allow_blank=False
+        )
+
+    @on(Select.Changed)
+    def show_or_hide(self, event: Select.Changed) -> None:
+        assert isinstance(event.value, bool)
+        self.query_one(Label).set_class(not event.value, "hidden")
+
+if __name__ == "__main__":
+    HideAndSeek().run()
+
+### hide_and_seek.py ends here
