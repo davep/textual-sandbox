c5ccdc1a3dfbf53805bf64b336d3e65db66c2a0c
Dave Pearson <davep@davep.org>
2023-11-04 18:19:04 +0000

    :sparkles: Example of the loading indicator in the middle of the screen
    For a question on Discord.

---
+++ li_in_middle.py
@@ -0,0 +1,39 @@
+from textual.app import App, ComposeResult
+from textual.widgets import LoadingIndicator, Footer, Static
+
+class LoadingApp(App[None]):
+
+    CSS = """
+    #one {
+        height: 1;
+    }
+
+    #two {
+        height: 2;
+    }
+
+    #three {
+        height: 3;
+    }
+
+    Static {
+        border: solid red;
+    }
+
+    LoadingIndicator {
+        height: 1fr;
+        border: solid green;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Static("One", id="one")
+        yield Static("Two", id="two")
+        yield Static("Three", id="three")
+        yield LoadingIndicator()
+        yield Footer()
+
+if __name__ == "__main__":
+    LoadingApp().run()
+
+### li_in_middle.py ends here
