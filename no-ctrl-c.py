6bec6ad4bb6ce560f06a338de5274fd066b3817b
Dave Pearson <davep@davep.org>
2023-11-11 12:37:15 +0000

    :sparkles: Example of using a different key to quit
    For a question on Discord.

---
+++ no-ctrl-c.py
@@ -0,0 +1,18 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Label
+
+class NoCtrlCApp(App[None]):
+
+    BINDINGS = [
+        ("ctrl+q", "quit"),
+        ("ctrl+c", "gndn")
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Label("Press Ctrl+Q to quit")
+        yield Footer()
+
+if __name__ == "__main__":
+    NoCtrlCApp().run()
+
+### no-ctrl-c.py ends here
