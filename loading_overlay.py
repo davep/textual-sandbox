3545b8cdfeb4b9ae60950fe637a3066f06c4c635
Dave Pearson <davep@davep.org>
2023-11-14 15:35:20 +0000

    :sparkles: Example of https://github.com/Textualize/textual/issues/3677

---
+++ loading_overlay.py
@@ -0,0 +1,30 @@
+"""https://github.com/Textualize/textual/issues/3677"""
+
+from textual.app import App, ComposeResult
+from textual.widgets import Label
+
+class LoadingOverlayApp(App[None]):
+
+    CSS = """
+    Label {
+        width: 90%;
+        height: 1fr;
+        text-align: center;
+        border: solid red;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Label("This is a really big label")
+
+    def on_mount(self) -> None:
+        self.notify(
+            "This is a really big notification. Loading should not overwrite it.",
+            timeout=9_999_999
+        )
+        self.query_one(Label).loading = True
+
+if __name__ == "__main__":
+    LoadingOverlayApp().run()
+
+### loading_overlay.py ends here
