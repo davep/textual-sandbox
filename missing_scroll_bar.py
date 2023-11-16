550711173d653973401238d315ad5630d931f3ff
Dave Pearson <davep@davep.org>
2023-11-16 08:42:19 +0000

    :sparkles: Example for https://github.com/Textualize/textual/issues/3687

---
+++ missing_scroll_bar.py
@@ -0,0 +1,37 @@
+"""https://github.com/Textualize/textual/issues/3687"""
+
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal
+from textual.widgets import OptionList
+
+class MissingScrollbarApp(App[None]):
+
+    CSS = """
+    OptionList {
+        height: 1fr;
+    }
+
+    #left {
+        min-width: 25;
+    }
+
+    #middle {
+        width: 5fr;
+    }
+
+    #right {
+        min-width: 30;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        options = [str(n) for n in range(200)]
+        with Horizontal():
+            yield OptionList(*options, id="left")
+            yield OptionList(*options, id="middle")
+            yield OptionList(*options, id="right")
+
+if __name__ == "__main__":
+    MissingScrollbarApp().run()
+
+### missing_scroll_bar.py ends here
