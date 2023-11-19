fc1d2085b42a92bb2fb4618178e5607ad37fdb8c
Dave Pearson <davep@davep.org>
2023-11-19 16:28:04 +0000

    :sparkles: Add a display of the DOM to the example

--- tc_fun.py
+++ tc_fun.py
@@ -1,7 +1,8 @@
 """https://github.com/Textualize/textual/issues/3695"""
 
 from textual.app import App, ComposeResult
-from textual.widgets import TabbedContent, TabPane, Label
+from textual.containers import Horizontal
+from textual.widgets import TabbedContent, TabPane, Label, Static
 
 class TabContentFun(App[None]):
 
@@ -13,14 +14,23 @@ class TabContentFun(App[None]):
     ContentTab#second {
         color: green;
     }
+
+    TabbedContent, #tree {
+        width: 1fr;
+    }
     """
 
     def compose(self) -> ComposeResult:
-        with TabbedContent():
-            with TabPane("First", id="first"):
-                yield Label("First")
-            with TabPane("Second", id="second"):
-                yield Label("second")
+        with Horizontal():
+            with TabbedContent():
+                with TabPane("First", id="first"):
+                    yield Label("First")
+                with TabPane("Second", id="second"):
+                    yield Label("Second")
+            yield Static(id="tree")
+
+    def on_mount(self) -> None:
+        self.query_one("#tree", Static).update(self.query_one(TabbedContent).tree)
 
 if __name__ == "__main__":
     TabContentFun().run()
