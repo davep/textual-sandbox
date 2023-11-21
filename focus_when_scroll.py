af1ca54b251a5c302c4120c41fbbceed0ddcb85a
Dave Pearson <davep@davep.org>
2023-11-21 14:47:13 +0000

    :sparkles: Example for https://github.com/Textualize/textual/discussions/3717

---
+++ focus_when_scroll.py
@@ -0,0 +1,35 @@
+from textual import on
+from textual.app import App, ComposeResult
+from textual.containers import VerticalScroll
+from textual.widgets import Button, Label
+
+class MaybeFocus(VerticalScroll, can_focus=False):
+
+    def watch_show_vertical_scrollbar(self) -> None:
+        self.can_focus = self.show_horizontal_scrollbar or self.show_vertical_scrollbar
+
+    def watch_show_horizontal_scrollbar(self) -> None:
+        self.can_focus = self.show_horizontal_scrollbar or self.show_vertical_scrollbar
+
+class FocusWhenScrollApp(App[None]):
+
+    CSS = """
+    Label {
+        height: 5;
+        background: red;
+        width: 1fr;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Button("MORE!")
+        yield MaybeFocus()
+
+    @on(Button.Pressed)
+    def more(self) -> None:
+        self.query_one(MaybeFocus).mount(Label("Stuff"))
+
+if __name__ == "__main__":
+    FocusWhenScrollApp().run()
+
+### focus_when_scroll.py ends here
