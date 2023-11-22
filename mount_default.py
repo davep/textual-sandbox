ea959da9c098abc213184eee36cbc0c33eb82552
Dave Pearson <davep@davep.org>
2023-11-22 09:35:25 +0000

    :sparkles: Add an example of default behaviour

---
+++ mount_default.py
@@ -0,0 +1,25 @@
+"""Example of default behaviour."""
+
+from textual.app import App, ComposeResult
+from textual.widgets import Static, Log
+
+class Parent(Static):
+
+    def on_mount(self) -> None:
+        self.app.query_one(Log).write_line("This is from Parent.on_mount")
+
+class Child(Parent):
+
+    def on_mount(self) -> None:
+        self.app.query_one(Log).write_line("This is from Child.on_mount")
+
+class OnMountDefaultApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield Child()
+        yield Log()
+
+if __name__ == "__main__":
+    OnMountDefaultApp().run()
+
+### mount_default.py ends here
