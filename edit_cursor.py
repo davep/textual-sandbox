85db52ab22b051d933dc5b25bd22858e549132d4
Dave Pearson <davep@davep.org>
2023-11-22 09:34:52 +0000

    :sparkles: Add illustration of https://github.com/Textualize/textual/issues/3722

---
+++ edit_cursor.py
@@ -0,0 +1,40 @@
+"""https://github.com/Textualize/textual/issues/3722"""
+
+from textual.app import App, ComposeResult
+from textual.events import Key
+from textual.widgets import Input, TextArea
+
+class TabbyTextArea(TextArea):
+
+    DEFAULT_CSS = """
+    TabbyTextArea {
+        background: $boost;
+        color: $text;
+        padding: 0 2;
+        border: tall $background;
+    }
+
+    TabbyTextArea:focus {
+        border: tall $accent;
+    }
+    """
+
+    def on_mount(self) -> None:
+        self.show_line_numbers = False
+
+    async def _on_key(self, event: Key) -> None:
+        if event.key == "tab":
+            event.prevent_default()
+            self.screen.focus_next()
+
+class CursorApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        for _ in range(5):
+            yield Input()
+            yield TabbyTextArea()
+
+if __name__ == "__main__":
+    CursorApp().run()
+
+### edit_cursor.py ends here
