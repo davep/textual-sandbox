b41054ce5bc89b7236d64c1fb2eac12941310899
Dave Pearson <davep@davep.org>
2023-11-22 19:44:55 +0000

    :sparkles: Add example of odd Input sizeing issue

---
+++ input_vs_textarea.py
@@ -0,0 +1,23 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Input, TextArea, Static, Button
+
+class InputVsTextArea(App[None]):
+
+    CSS = """
+    Screen > *, Screen > *:focus {
+        width: 50%;
+        height: 1fr;
+        border: solid red;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Input()
+        yield TextArea()
+        yield Static()
+        yield Button()
+
+if __name__ == "__main__":
+    InputVsTextArea().run()
+
+### input_vs_textarea.py ends here
