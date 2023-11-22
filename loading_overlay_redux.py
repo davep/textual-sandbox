43a4b629e9aae6a758a1cc0c5ea49503dca00f84
Dave Pearson <davep@davep.org>
2023-11-22 20:00:33 +0000

    :sparkles: Example for https://github.com/Textualize/textual/issues/3733

---
+++ loading_overlay_redux.py
@@ -0,0 +1,31 @@
+"""https://github.com/Textualize/textual/issues/3733"""
+
+from textual.app import App, ComposeResult
+from textual.widgets import OptionList
+
+class LoadingOverlayRedux(App[None]):
+
+    CSS = """
+    OptionList {
+        scrollbar-gutter: stable;
+        width: 1fr;
+        height: 1fr;
+    }
+    """
+
+    BINDINGS = [
+        ("space", "toggle")
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield OptionList(*[
+            ("X" * 500) for _ in range(1_000)
+        ])
+
+    def action_toggle(self) -> None:
+        self.query_one(OptionList).loading = not self.query_one(OptionList).loading
+
+if __name__ == "__main__":
+    LoadingOverlayRedux().run()
+
+### loading_overlay_redux.py ends here
