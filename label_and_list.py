687d3d980583da8172c16936586f1e56e3b20375
Dave Pearson <davep@davep.org>
2023-11-26 18:47:24 +0000

    :sparkles: Example of centering a label and a listbox
    For a question on Discord.

---
+++ label_and_list.py
@@ -0,0 +1,34 @@
+from textual.app import App, ComposeResult
+from textual.containers import Center
+from textual.widgets import Label, ListView, ListItem
+
+class LabelAndListViewApp(App[None]):
+
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+
+    ListView {
+        width: 30;
+        margin: 2 2;
+        height: auto;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        with Center():
+            yield Label("What's the best SciFi show ever?")
+        with Center():
+            yield ListView(
+                ListItem(Label("Firefly?")),
+                ListItem(Label("Firefly!")),
+                ListItem(Label("Firefly!!")),
+                ListItem(Label("Firefly!!!")),
+                ListItem(Label("Firefly!!!!")),
+            )
+
+if __name__ == "__main__":
+    LabelAndListViewApp().run()
+
+### label_and_list.py ends here
