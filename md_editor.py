d8ab321dbf4bbfb94ccf68562f1d11fd42577f85
Dave Pearson <davep@davep.org>
2023-11-29 07:52:04 +0000

    :sparkles: Example of live Markdown preview

---
+++ md_editor.py
@@ -0,0 +1,30 @@
+"""A minimal Markdown live preview scratchpad."""
+
+from textual import on
+from textual.app import App, ComposeResult
+from textual.widgets import TextArea, Markdown
+
+class MDEditor(App[None]):
+
+    CSS = """
+    Screen {
+        layout: horizontal;
+    }
+
+    Screen > * {
+        width: 1fr;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield TextArea(language="markdown")
+        yield Markdown()
+
+    @on(TextArea.Changed)
+    def update_preview(self, event: TextArea.Changed) -> None:
+        self.query_one(Markdown).update(event.text_area.text)
+
+if __name__ == "__main__":
+    MDEditor().run()
+
+### md_editor.py ends here
