2a62e3afcad3136792ab64b17b9f8bff1152e87a
Dave Pearson <davep@davep.org>
2023-11-30 10:03:56 +0000

    :sparkles: Example of setting a label of a button in a radioset

---
+++ radio_change.py
@@ -0,0 +1,23 @@
+"""Example of changing a radioset's button's label."""
+
+from textual.app import App, ComposeResult
+from textual.widgets import RadioSet, RadioButton
+
+class RadioLabelsApp(App[None]):
+
+    BINDINGS = [("s", "swap")]
+
+    def compose(self) -> ComposeResult:
+        yield RadioSet(
+            RadioButton("Alpha", id="one"),
+            RadioButton("Beta", id="two")
+        )
+
+    def action_swap(self) -> None:
+        self.query_one("#one", RadioButton).label = "Gamma"
+        self.query_one("#two", RadioButton).label = "Delta"
+
+if __name__ == "__main__":
+    RadioLabelsApp().run()
+
+### radio_change.py ends here
