725041e661dfe5f3e298b4ded17d5774a43aa585
Dave Pearson <davep@davep.org>
2023-12-05 10:00:53 +0000

    :sparkles: Timer example for a question in Discord

---
+++ timer_example.py
@@ -0,0 +1,18 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Label
+
+class BoomApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield Label()
+
+    def big_bada_boom(self) -> None:
+        self.query_one(Label).update("BOOM!")
+
+    def on_mount(self) -> None:
+        self.set_timer(3, self.big_bada_boom)
+
+if __name__ == "__main__":
+    BoomApp().run()
+
+### timer_example.py ends here
