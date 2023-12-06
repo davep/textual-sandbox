6fd80cbc7cbb4970a8cc6f256a01e118017f5720
Dave Pearson <davep@davep.org>
2023-12-06 13:57:48 +0000

    :sparkles: More helper code for debugging TabbedContent

--- tc_query_issue.py
+++ tc_query_issue.py
@@ -2,9 +2,16 @@
 
 from textual import on
 from textual.app import App, ComposeResult
-from textual.widgets import Button, TabbedContent, TabPane, Label
+from textual.containers import Horizontal
+from textual.widgets import Button, TabbedContent, TabPane, Label, Log, Static
 
 class TabbedDiscontentApp(App[None]):
+
+    CSS = """
+    #info > * {
+        width: 1fr;
+    }
+    """
 
     def compose(self) -> ComposeResult:
         with TabbedContent():
@@ -12,12 +19,25 @@ class TabbedDiscontentApp(App[None]):
                 with TabPane(f"Tab {n}", id=f"test-{n}"):
                     yield Label(f"Hello from test pane {n}")
         yield Button("Query TabPane 1")
+        with Horizontal(id="info"):
+            yield Static(id="dom")
+            yield Log()
+
+    def on_mount(self) -> None:
+        self.query_one("#dom", Static).update(
+            self.query_one(TabbedContent).tree
+        )
 
     @on(Button.Pressed)
     def test_it(self) -> None:
         self.notify(
             f"{self.query_one('#test-1')!r}"
         )
+
+    @on(TabbedContent.TabActivated)
+    def logger(self, event: TabbedContent.TabActivated) -> None:
+        self.query_one(Log).write_line(f"{event!r}")
+        self.query_one(Log).write_line(f"Active is '{self.query_one(TabbedContent).active}'")
 
 if __name__ == "__main__":
     TabbedDiscontentApp().run()
