ec72b8b8c5316c0ed7ac06e7ce343b43428d38da
Dave Pearson <davep@davep.org>
2023-12-06 19:10:01 +0000

    :sparkles: Example of a auto-height TextArea with a max size
    For a question on Discord.

---
+++ max_edit.py
@@ -0,0 +1,29 @@
+"""Auto-size TextArea with a max size."""
+
+from textual.app import App, ComposeResult
+from textual.containers import Vertical
+from textual.widgets import TextArea
+
+class MaxHeightEdit(App[None]):
+
+    CSS = """
+    Vertical {
+        border: red;
+        height: 50%;
+    }
+
+    TextArea {
+        height: auto;
+        min-height: 5;
+        max-height: 1fr;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        with Vertical():
+            yield TextArea()
+
+if __name__ == "__main__":
+    MaxHeightEdit().run()
+
+### max_edit.py ends here
