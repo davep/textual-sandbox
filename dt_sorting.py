fd99e9ac4d55f84eae29ae9659dd9f3005486476
Dave Pearson <davep@davep.org>
2023-12-10 07:43:52 +0000

    :sparkles: DataTable sorting example

---
+++ dt_sorting.py
@@ -0,0 +1,36 @@
+"""DataTable sorting example for a question on Discord."""
+
+from textual.app import App, ComposeResult
+from textual.widgets import DataTable
+
+ROWS = [
+    ("lane", "swimmer", "country", "time"),
+    (4, "Joseph Schooling", "Singapore", 50.39),
+    (2, "Michael Phelps", "United States", 51.14),
+    (5, "Chad le Clos", "South Africa", 51.14),
+    (6, "László Cseh", "Hungary", 51.14),
+    (3, "Li Zhuhao", "China", 51.26),
+    (8, "Mehdy Metella", "France", 51.58),
+    (7, "Tom Shields", "United States", 51.73),
+    (1, "Aleksandr Sadovnikov", "Russia", 51.84),
+    (10, "Darren Burns", "Scotland", 51.84),
+]
+
+
+class TableApp(App):
+    def compose(self) -> ComposeResult:
+        yield DataTable()
+
+    def on_mount(self) -> None:
+        table = self.query_one(DataTable)
+        for column in ROWS[0]:
+            table.add_column(column, key=column)
+        table.add_rows(ROWS[1:])
+        table.sort("time")
+
+
+app = TableApp()
+if __name__ == "__main__":
+    app.run()
+
+### dt_sorting.py ends here
