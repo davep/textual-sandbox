8ce08b66b9cb032d12886ff62631663cb0b0b557
Dave Pearson <davep@davep.org>
2023-12-17 09:57:20 +0000

    :sparkles: Example for https://github.com/Textualize/textual/discussions/3889

---
+++ bound_focus.py
@@ -0,0 +1,36 @@
+"""https://github.com/Textualize/textual/discussions/3889"""
+
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal, Vertical
+from textual.widgets import Label
+
+class Panel(Vertical, can_focus=True):
+
+    DEFAULT_CSS = """
+    Panel {
+        border: round red;
+    }
+
+    Panel:focus {
+        border: round green;
+    }
+    """
+
+class BoundFocusExampleApp(App[None]):
+
+    BINDINGS = [
+        ("f1", "focus('left')"),
+        ("f2", "focus('right')"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            with Panel(id="left"):
+                yield Label("This is the left panel (press F1)")
+            with Panel(id="right"):
+                yield Label("This is the right panel (press F2)")
+
+if __name__ == "__main__":
+    BoundFocusExampleApp().run()
+
+### bound_focus.py ends here
