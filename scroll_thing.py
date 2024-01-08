17250bb59dc398d2dbcf662196563ac0e33e4ec1
Dave Pearson <davep@davep.org>
2024-01-08 09:21:05 +0000

    :sparkles: Example of scrolling a widget with reaction tool
    For a question on Discord.

---
+++ scroll_thing.py
@@ -0,0 +1,53 @@
+"""Example for a question on Discord."""
+
+from rich.text import Text
+
+from textual.app import App, ComposeResult, RenderResult
+from textual.reactive import reactive
+from textual.containers import VerticalScroll
+from textual.widget import Widget
+
+class TextDisplayWidget(Widget):
+
+    DEFAULT_CSS = """
+    TextDisplayWidget {
+        height: auto;
+    }
+    """
+
+    line_count: reactive[int] = reactive(500, layout=True)
+    line_prefix: reactive[str] = reactive("This is an example")
+
+    def render(self) -> RenderResult:
+        return Text("\n".join(f"{self.line_prefix} {n}" for n in range(self.line_count)))
+
+class ScrollTextThingApp(App[None]):
+
+    BINDINGS = [
+        ("1", "lines(1)"),
+        ("2", "lines(2)"),
+        ("3", "lines(3)"),
+        ("4", "lines(4)"),
+        ("5", "lines(5)"),
+        ("6", "lines(6)"),
+        ("7", "lines(7)"),
+        ("8", "lines(8)"),
+        ("9", "lines(9)"),
+        ("f1", "text('Hey, you pressed F1!')"),
+        ("f2", "text('So this reactive thing is kinda cool right?')"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        with VerticalScroll():
+            yield TextDisplayWidget()
+
+    def action_lines(self, lines: int) -> None:
+        self.query_one(TextDisplayWidget).line_count = 10 * lines
+
+    def action_text(self, text: str) -> None:
+        self.query_one(TextDisplayWidget).line_prefix = text
+
+if __name__ == "__main__":
+    ScrollTextThingApp().run()
+
+### scroll_thing.py ends here
