885218f3c600c83d6d3ef469b76cc6e2e3bf7b0a
Dave Pearson <davep@davep.org>
2024-01-21 16:12:54 +0000

    :sparkles: Add an example of using a LoadingIndicator as a modal
    For a question on Discord.

---
+++ modal_loading.py
@@ -0,0 +1,35 @@
+"""Example of using a loading indicator as a loading modal."""
+
+from textual.app import App, ComposeResult
+from textual.screen import ModalScreen
+from textual.widgets import LoadingIndicator
+
+class LoadingScreen(ModalScreen[None]):
+
+    CSS = """
+    LoadingScreen {
+        align: center middle;
+        LoadingIndicator {
+            width: 50%;
+            height: 3;
+        }
+    }
+    """
+    def compose(self) -> ComposeResult:
+        yield LoadingIndicator()
+
+class LoadingIndicatorModalScreenApp(App[None]):
+
+    CSS = """
+    Screen#_default {
+        background: red;
+    }
+    """
+
+    def on_mount(self) -> None:
+        self.push_screen(LoadingScreen())
+
+if __name__ == "__main__":
+    LoadingIndicatorModalScreenApp().run()
+
+### modal_loading.py ends here
