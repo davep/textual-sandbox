efe018e4fd8cf7731a21b1af288bb7351a7f8a52
Dave Pearson <davep@davep.org>
2024-01-29 15:46:16 +0000

    :sparkles: More signal testing

--- signal_tester.py
+++ signal_tester.py
@@ -1,33 +1,47 @@
 """Suspend-related signal testing."""
 
 from os import kill, getpid
-from signal import SIGTTOU, signal, SIGTTIN, SIGCONT, SIGTSTP, SIGSTOP
+from sys import __stdout__ as stdout
+from signal import SIGTTOU, signal, SIGTTIN, SIGCONT, SIGTSTP, SIGSTOP, SIG_DFL
+from termios import tcgetattr, tcsetattr, TCSANOW, error
 
-def sigttin_handler(*_) -> None:
-    print("SIGTTIN - This means we got placed in the bg and we're trying to get input")
-    print("SIGTTIN - We can't keep doing that so we're going to send SIGSTOP again")
+def disallowed_resume(*_) -> None:
     kill(getpid(), SIGSTOP)
 
-def sigttou_handler(*_) -> None:
-    print("SIGTTOU")
+def alt_screen(enable: bool) -> None:
+    if enable:
+        signal(SIGTTOU, disallowed_resume)
+        signal(SIGTTIN, disallowed_resume)
+        try:
+            tcsetattr(stdout.fileno(), TCSANOW, tcgetattr(stdout.fileno()))
+        except error:
+            return
+        finally:
+            signal(SIGTTOU, SIG_DFL)
+            signal(SIGTTIN, SIG_DFL)
+    print(
+        "\x1b[?1049h\x1b[?25l" if enable else "\x1b[?1049l\x1b[?25h"
+    )
 
 def sigcont_handler(*_) -> None:
-    print("SIGCONT - We're back! This will happen before the final line in SIGTSTP")
+    print("CONT")
+    alt_screen(True)
 
 def sigtstp_handler(*_) -> None:
-    print("SIGTSTP - This is where we'd pause the Textual app")
-    print("SIGTSTP - Now that we've done that, we'll send SIGSTOP itself to the app")
+    alt_screen(False)
     kill(getpid(), SIGSTOP)
-    print("SIGTSTP - THis is after: kill(getpid(), SIGSTOP)")
 
 def run() -> None:
+    alt_screen(True)
     signal(SIGTSTP, sigtstp_handler)
-    signal(SIGTTIN, sigttin_handler)
-    signal(SIGTTOU, sigttou_handler)
     signal(SIGCONT, sigcont_handler)
     for n in range(10_000):
         print(n)
-        input("Press Enter")
+        try:
+            input("Press Enter")
+        except (EOFError, KeyboardInterrupt):
+            break
+    alt_screen(False)
 
 if __name__ == "__main__":
     run()
