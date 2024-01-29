cddc0351657533e9ad9d22674ed74c699057aa91
Dave Pearson <davep@davep.org>
2024-01-29 09:32:50 +0000

    :sparkles: Flesh out the signal tester some more

--- signal_tester.py
+++ signal_tester.py
@@ -1,21 +1,29 @@
 """Suspend-related signal testing."""
 
 from os import kill, getpid
-from signal import signal, SIGTTIN, SIGCONT, SIGTSTP, SIGSTOP
+from signal import SIGTTOU, signal, SIGTTIN, SIGCONT, SIGTSTP, SIGSTOP
 
 def sigttin_handler(*_) -> None:
-    print("SIGTTIN")
+    print("SIGTTIN - This means we got placed in the bg and we're trying to get input")
+    print("SIGTTIN - We can't keep doing that so we're going to send SIGSTOP again")
+    kill(getpid(), SIGSTOP)
+
+def sigttou_handler(*_) -> None:
+    print("SIGTTOU")
 
 def sigcont_handler(*_) -> None:
-    print("SIGCONT")
+    print("SIGCONT - We're back! This will happen before the final line in SIGTSTP")
 
 def sigtstp_handler(*_) -> None:
-    print("SIGTSTP")
+    print("SIGTSTP - This is where we'd pause the Textual app")
+    print("SIGTSTP - Now that we've done that, we'll send SIGSTOP itself to the app")
     kill(getpid(), SIGSTOP)
+    print("SIGTSTP - THis is after: kill(getpid(), SIGSTOP)")
 
 def run() -> None:
     signal(SIGTSTP, sigtstp_handler)
     signal(SIGTTIN, sigttin_handler)
+    signal(SIGTTOU, sigttou_handler)
     signal(SIGCONT, sigcont_handler)
     for n in range(10_000):
         print(n)
