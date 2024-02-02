8e3c342aabf8e4315c2a6040fb97af4dc5119409
Dave Pearson <davep@davep.org>
2024-02-02 09:11:51 +0000

    :sparkles: Example for https://github.com/Textualize/textual/issues/4088

---
+++ await_type_warning.py
@@ -0,0 +1,12 @@
+"""https://github.com/Textualize/textual/issues/4088"""
+
+from textual.app import App
+from textual.widgets import TabbedContent, TabPane
+
+class AwaitableTypeWarningApp(App[None]):
+
+    async def on_mount(self) -> None:
+        await self.query_one(TabbedContent).add_pane(TabPane("Test"))
+        await self.query_one(TabbedContent).remove_pane("some-tab")
+
+### await_type_warning.py ends here
