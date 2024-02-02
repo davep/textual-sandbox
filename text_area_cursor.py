62c952433983fa364564d60a70f8c18f0c939b7a
Dave Pearson <davep@davep.org>
2024-02-02 21:10:57 +0000

    :sparkles: Example for https://github.com/Textualize/textual/issues/4109

---
+++ text_area_cursor.py
@@ -0,0 +1,15 @@
+"""https://github.com/Textualize/textual/issues/4109"""
+
+from textual.app import App, ComposeResult
+from textual.widgets import Input, TextArea
+
+class TextAreaCursorApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield Input()
+        yield TextArea()
+
+if __name__ == "__main__":
+    TextAreaCursorApp().run()
+
+### text_area_cursor.py ends here
