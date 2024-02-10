a86be09355fcef32c261749cc48c54722633fd04
Dave Pearson <davep@davep.org>
2024-02-10 08:51:54 +0000

    :sparkles: Example of an option selected event
    For a question on Discord.

---
+++ option_selected.py
@@ -0,0 +1,38 @@
+"""Example of handling an option selected event."""
+
+from textual import on
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header, OptionList, Label
+
+
+class OptionListApp(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield OptionList(
+            "Aerilon",
+            "Aquaria",
+            "Canceron",
+            "Caprica",
+            "Gemenon",
+            "Leonis",
+            "Libran",
+            "Picon",
+            "Sagittaron",
+            "Scorpia",
+            "Tauron",
+            "Virgon",
+        )
+        yield Label("Nothing selected yet")
+        yield Footer()
+
+    @on(OptionList.OptionSelected)
+    def test_function(self, event: OptionList.OptionSelected) -> None:
+        self.query_one(Label).update(
+            f"You selected {event.option_index}"
+        )
+
+if __name__ == "__main__":
+    OptionListApp().run()
+
+### option_selected.py ends here
