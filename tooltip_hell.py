fba9fb15b7cdc6f335fccb16d6dfda81ca4f9f82
Dave Pearson <davep@davep.org>
2024-02-20 10:43:34 +0000

    :zap: Add a tooltip test for the display styling too

--- tooltip_hell.py
+++ tooltip_hell.py
@@ -23,7 +23,7 @@ class Buttonatron(Grid, GoodTipper, can_focus=True):
     Buttonatron {
         height: 1fr;
         grid-size: 5;
-        grid-rows: 5;
+        grid-rows: 7;
 
         Button {
            width: 1fr;
@@ -47,10 +47,10 @@ class Buttonatron(Grid, GoodTipper, can_focus=True):
     def remove_button(self, event: Button.Pressed) -> None:
         event.control.remove()
 
-class HideAndShow(Container, GoodTipper, can_focus=True):
+class VisibleToggle(Container, GoodTipper, can_focus=True):
 
     DEFAULT_CSS = """
-    HideAndShow {
+    VisibleToggle {
         Label {
             width: 1fr;
             height: 1fr;
@@ -64,12 +64,37 @@ class HideAndShow(Container, GoodTipper, can_focus=True):
 
     BINDINGS = [("space", "toggle_label")]
 
-
     def compose(self) -> ComposeResult:
-        yield self.tip(Label("Press space to hide/show me!"))
+        yield self.tip(Label("Press space to toggle my visible attribute!"))
 
     def action_toggle_label(self) -> None:
         self.query_one(Label).visible = not self.query_one(Label).visible
+
+class DisplayToggle(Container, GoodTipper, can_focus=True):
+
+    DEFAULT_CSS = """
+    DisplayToggle {
+        Label {
+            width: 1fr;
+            height: 1fr;
+            border: solid red;
+            color: $text;
+            background: yellow;
+            content-align: center middle;
+        }
+    }
+    """
+
+    BINDINGS = [("space", "toggle_label")]
+
+    def compose(self) -> ComposeResult:
+        yield self.tip(Label("Press space to toggle my display attribute!"))
+
+    def action_toggle_label(self) -> None:
+        self.query_one(Label).styles.display = (
+            "none" if self.query_one(Label).styles.display == "block" else "block"
+        )
+
 
 class LabelsAllTheWayDown(VerticalScroll, GoodTipper, can_focus=True):
 
@@ -98,7 +123,7 @@ class LabelsAllTheWayDown(VerticalScroll, GoodTipper, can_focus=True):
 class TooltipHellApp(App[None]):
 
     CSS = """
-    Buttonatron, LabelsAllTheWayDown, HideAndShow {
+    Buttonatron, LabelsAllTheWayDown, VisibleToggle, DisplayToggle {
         border: blank;
         &:focus {
             border: thick green;
@@ -107,10 +132,12 @@ class TooltipHellApp(App[None]):
     """
 
     def compose(self) -> ComposeResult:
-        yield Buttonatron()
         with Horizontal():
+            yield Buttonatron()
             yield LabelsAllTheWayDown()
-            yield HideAndShow()
+        with Horizontal():
+            yield VisibleToggle()
+            yield DisplayToggle()
 
 if __name__ == "__main__":
     TooltipHellApp().run()
