65d33261639d9c9b34521775eab7cd63d8bb6f51
Dave Pearson <davep@davep.org>
2024-04-14 19:20:39 +0100

    :sparkles: Explicitly show how to swap out the DirTree "icon"

--- dir_tree_labels.py
+++ dir_tree_labels.py
@@ -1,5 +1,7 @@
 """Example of adding extra stuff to a DirectoryTree entry."""
 
+from pathlib import Path
+
 from rich.style import Style
 from rich.text import Text
 from textual.app import App, ComposeResult
@@ -8,18 +10,30 @@ from textual.widgets import DirectoryTree
 from textual.widgets.tree import TreeNode
 
 
-class MyDirectoryTree(DirectoryTree):
+class DirectoryTreeEx(DirectoryTree):
+    def file_icon(self, path: Path) -> str:
+        return {
+            ".py": "🐍",
+            ".rst": "🦀",
+            # etc...
+        }.get(path.suffix.lower(), "📄")
+
     def render_label(
         self, node: TreeNode[DirEntry], base_style: Style, style: Style
     ) -> Text:
-        return Text.assemble(
-            super().render_label(node, base_style, style), " LOOK I ADDED THIS MYSELF!"
-        )
+        # Get the label from DirectoryTree
+        label = super().render_label(node, base_style, style)
+        # If this isn't a directory...
+        if node.data is not None and not node._allow_expand:
+            # ...strip off the plain old paper sheet icon and add one
+            # depending on the extension...
+            label = Text.assemble(self.file_icon(node.data.path), label.divide([1])[1])
+        return label
 
 
 class DirTreeLabelsApp(App[None]):
     def compose(self) -> ComposeResult:
-        yield MyDirectoryTree(".")
+        yield DirectoryTreeEx(".")
 
 
 if __name__ == "__main__":
